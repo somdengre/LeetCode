9686a39dcd0b0991e0ba5bc208ece9d7324e5586
somdengre <115890508+somdengre@users.noreply.github.com>
2023-01-19 18:01:37 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (85.56%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        priority_queue<int> pq;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] != 0)
+            {
+                if(pq.empty())
+                {
+                    pq.push(nums[i]);
+                }
+                else
+                {
+                    if(pq.top() == nums[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        pq.push(nums[i]);
+                    }
+                }
+            }
+        }
+        return pq.size();
+    }
+};
