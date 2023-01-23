8502f5a2e4088011408c0e0a12d87a850e3f1f61
somdengre <115890508+somdengre@users.noreply.github.com>
2023-01-23 17:28:15 +0530

    Time: 3 ms (40.87%), Space: 6 MB (30.22%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string s;
+        for(int i=0;i<address.size();i++){
+            if(address[i]=='.'){
+            s=s+"[.]";
+                
+        }
+        else{
+            s=s+address[i];
+            
+        }
+        }
+        
+        
+        return s;
+        
+    }
+};
