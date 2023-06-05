ba61f022b78f557e99361f283bd48741ed456771
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-06-06 01:54:01 +0530

    Time: 4 ms (63.13%), Space: 7 MB (56.68%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        if(s.length()==0) return 0;
+        int i=0;
+        
+        
+        while(i<s.length() && s[i]==' ') i++;
+        
+       
+        s=s.substr(i);
+        int sign=+1;
+        long ans=0;
+        
+        if(s[0]=='-') sign = -1;
+        
+        int maxi=INT_MAX, mini=INT_MIN;
+
+        
+        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
+        
+        
+        while(i < s.length()){
+            
+            if(s[0] == ' ' || !isdigit(s[i])) break;
+           
+            ans = ans*10+s[i] - '0';
+            
+            if(sign == -1 && -1*ans < mini) return mini;
+            if(sign == +1 && ans > maxi) return maxi;
+            
+            i++;
+        }
+        return (int)(sign*ans);
+    }
+};
