1d8e759de76e72aab5febfc74f87307077ea203f
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-06-09 15:51:40 +0530

    Time: 19 ms (25.60%), Space: 15.5 MB (75.91%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
+    int binary_search(int num,int l,int r,vector<int>& numbers, int target){ 
+        int mid;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(numbers[mid]==num) return mid;
+            else if(numbers[mid]>num) r=mid-1;
+            else l=mid+1;
+        }
+        return -1; 
+    }
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int i=0; 
-        int j=numbers.size()-1; 
         vector<int> ans;
-        while(i<j){
-            if(numbers[i]+numbers[j]==target){ 
+        for (int i=0;i<numbers.size();i++){
+            int j=binary_search(target-numbers[i],i+1,numbers.size()-1,numbers,target);
+            if(j!=-1){ 
                 ans.push_back(i+1);
                 ans.push_back(j+1);
                 return ans;
             }
-            if(numbers[i]+numbers[j]>target) j--; 
-            else i++; 
-        }
-        return ans;
+        }      
+        return ans; 
     }
+    
 };
