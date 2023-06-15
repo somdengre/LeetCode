a104331049181e704465bf5a179c3f5d6f54b68a
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-06-15 15:23:21 +0530

    Time: 3 ms (85.02%), Space: 9.5 MB (64.04%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& mat, int X) {
-        bool DTD = false;
-        int N=mat.size();
-        int M=mat[0].size();
-	   for(int i=0;i<N;++i){
-	       for(int j=0;j<M;++j){
-	           if(mat[i][j]==X){
-	               DTD=true;
-	           }
-	       }
-	   }
-	   return DTD;
+    
+    bool bin(vector<int> & arr, int l,int h,int t){
+        while(l<=h){
+            int m = l+(h-l)/2;
+            
+            if(arr[m]==t){
+                return true;
+            }else if(arr[m]>t){
+                h = m-1;
+            }else{
+                l = m+1;
+            }
+        }
+        return false;
+        
+        
+    };
+    bool searchMatrix(vector<vector<int>>& mat, int x) {
+        bool ans = false;
+        int h = mat[0].size()-1;
+        for(int i=0;i< mat.size();i++){
+            ans = bin(mat[i],0,h,x);
+            if(ans == true){
+                return true;
+            }
+        }
+        
+        return false;
     }
 };
