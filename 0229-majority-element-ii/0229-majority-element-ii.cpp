ea0210ee7ddfe450d822c22db63323fff3ff785a
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-06-15 13:02:33 +0530

    Time: 8 ms (94.15%), Space: 16 MB (8.15%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        unordered_map<int ,int> mp;
+        int n = nums.size();
+        for(int i=0;i< n;i++){
+            mp[nums[i]]++;
+        }
+        
+        for(auto x:mp){
+            if(x.second > n/3){
+                ans.push_back(x.first);
+            }
+        }
+        
+        return ans;
+        
+    }
+};
