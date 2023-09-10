b9b2598d4e004d442f7a21b811a0ae2b2b2ce3c4
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-09-10 22:20:36 +0530

    Time: 281 ms (16.99%), Space: 137.9 MB (12.64%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* doubleIt(ListNode* head) {
+        ListNode *p = head;
+        vector<int> ans;
+        
+        while(p){
+            ans.push_back(p->val);
+            p = p-> next;
+        }
+        
+        reverse(ans.begin(), ans.end());
+        int carry = 0;
+        for(int i =0; i<ans.size();i++){
+            int current = (ans[i]*2 + carry)%10;
+            carry = (ans[i]*2 + carry)/10;
+            ans[i] = current;
+            
+        }
+        
+        if(carry !=0){
+            ans.push_back(carry);
+        }
+        ListNode *temp = new ListNode(-1);
+        ListNode *toReturn = temp;
+        for(int i = ans.size() - 1; i >= 0 ; i--){
+            ListNode * curr = new ListNode(ans[i]);
+            temp->next = curr;
+            temp = temp->next;
+        }
+        return toReturn->next;
+        
+        
+    }
+};
+
+
