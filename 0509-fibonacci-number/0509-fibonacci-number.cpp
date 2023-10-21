3ae9417600f643fb586f929d7e3b4247ca331ca3
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-10-22 00:47:23 +0530

    Time: 9 ms (49.92%), Space: 6.2 MB (44.42%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
-    int pq(int n){
-        if(n==0||n==1){
-            return n;
-        }else{
-            return pq(n-1)+pq(n-2);        
-        }
-    }
     int fib(int n) {
+        if((n==1)||(n==0))return n;
         
-        int ans = pq(n);
-        return ans;
-        
-        
-        
+        return fib(n-1)+fib(n-2);
     }
 };
