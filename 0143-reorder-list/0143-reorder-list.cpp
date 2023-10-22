f08ca6d581ac6e503d4fd4b2ba7dc897085c94ec
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-10-22 23:15:07 +0530

    Time: 32 ms (38.91%), Space: 19 MB (14.72%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+       int z = 0;
+        ListNode* dummy = head;
+        stack<ListNode *> s;
+        
+        while(dummy != NULL){
+            z++;
+            s.push(dummy);
+            dummy = dummy -> next;
+            
+        }
+        
+        dummy = head;
+        
+        for(int i=0;i<z/2;i++){
+            ListNode* temp = dummy->next;
+            dummy -> next = s.top();
+            s.pop();
+            dummy = dummy->next;
+            dummy->next = temp;
+            dummy = temp;
+        }
+        
+        dummy->next = NULL;
+        
+        
+    }
+};
+
+// class Solution {
+// public:
+//     void reorderList(ListNode* head) {
+//         int size = 0;
+//         ListNode* dummy = head;
+//         stack <ListNode*> nodes;
+//         while (dummy != NULL) {
+//             nodes.push(dummy);
+//             size++;
+//             dummy = dummy->next;
+//         }
+//         dummy = head;
+//         for (int i = 0; i < size / 2; i++) {
+//             ListNode* temp = dummy->next;
+//             dummy->next = nodes.top();
+//             nodes.pop();
+//             dummy = dummy->next;
+//             dummy->next = temp;
+//             dummy = temp;
+//         }
+//         dummy->next = NULL;
+//     }
+// };
