86c794164fa3882d3945fe22f127ce3b6e5e04bb
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-10-27 23:53:33 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (66.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
  public:
   int climbStairs(int n) {
-    int prev1 = 1;  // dp[i - 1]
-    int prev2 = 1;  // dp[i - 2]
-
-    for (int i = 2; i <= n; ++i) {
-      const int dp = prev1 + prev2;
-      prev2 = prev1;
-      prev1 = dp;
-    }
-
-    return prev1;
+    int p = 1;
+    int p1 = 1;
+      
+      for(int i = 2;i<= n ;i++){
+          int dp = p+p1;
+          p1 = p;
+          p = dp;
+      }
+      
+      return p;
   }
 };
