eb29659ebf0b52ffa81026f9d2ee5989f3f8893d
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-10-31 22:48:26 +0530

    Time: 97 ms (15.16%), Space: 76.3 MB (69.22%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findArray(vector<int>& nums) {
         int x = nums[0];
         
-        for(int i=1;i<nums.size();i++){
+        for(int i = 1;i<nums.size();i++){
             nums[i] = x^nums[i];
             x^=nums[i];
         }
