f65cc64807571f60f2b65451545b51b0421b99a0
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-11-02 00:28:49 +0530

    Time: 4 ms (75.25%), Space: 11 MB (84.49%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,20 +1,40 @@
 class Solution {
     
 public: 
-    void find(int ind,int target,vector<int> &arr,vector<vector<int>> &ans, vector<int> &ds) {
+//     void find(int ind,int target,vector<int> &arr,vector<vector<int>> &ans, vector<int> &ds) {
+//         if(ind == arr.size()){
+//             if(target == 0){
+//                 ans.push_back(ds);
+//             }
+//             return;
+//         }
+        
+//         if(arr[ind]<=target){
+//             ds.push_back(arr[ind]);
+//             find(ind, target-arr[ind],arr,ans,ds);
+//             ds.pop_back();
+//         }
+//         find(ind+1, target,arr,ans,ds);
+//     }
+    
+    void find(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
         if(ind == arr.size()){
             if(target == 0){
                 ans.push_back(ds);
+                
             }
             return;
         }
-        
-        if(arr[ind]<=target){
+        if(arr[ind]<= target){
             ds.push_back(arr[ind]);
-            find(ind, target-arr[ind],arr,ans,ds);
+            find(ind,target-arr[ind],arr,ans,ds);
             ds.pop_back();
+            
         }
-        find(ind+1, target,arr,ans,ds);
+        
+        find(ind+1,target,arr,ans,ds);
+        
+        
     }
     
 public:
