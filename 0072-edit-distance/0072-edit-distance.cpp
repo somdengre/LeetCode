f126500ffdd8a71599889ff9b04b84d5d873c9b4
Som Dengre <115890508+somdengre@users.noreply.github.com>
2023-12-31 19:44:09 +0530

    Time: 24 ms (22.77%), Space: 9.4 MB (30.39%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+//     int f(int i,int j,string s1,string s2,vector<vector<int>>&dp){
+//         if(i==0)return j;
+//         if(j==0)return i;
+        
+//         if(dp[i][j] != -1)return dp[i][j];
+//         if(s1[i-1] == s2[j-1]){
+//             return f(i-1,j-1,s1,s2,dp);
+//         }
+        
+//         return  dp[i][j] = 1+ min(f(i-1,j,s1,s2,dp), min(f(i-1,j,s1,s2,dp), f(i-1,j-1,s1,s2,dp)));
+//     }
+    int minDistance(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+         for(int i = 0;i<=n;i++)dp[i][0] = i;
+        for(int j = 0;j<=m;j++)dp[0][j] = j;
+       
+        
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] =  dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1+ min(dp[i-1][j] , min(dp[i][j-1] ,dp[i-1][j-1]));
+                }
+        
+   
+            }
+        }
+        return dp[n][m];
+    }
+};
