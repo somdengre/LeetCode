29b4994d5c2c6c1f9e475d2bb94c9c6dfb335cfa
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-01-13 12:38:19 +0530

    Time: 78 ms (39.88%), Space: 16.9 MB (62.45%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,25 +1,22 @@
-
 class Solution {
 public:
-
     int minSteps(string s, string t) {
-       unordered_map<char,int> mp;
-       for(auto i:s)
-       {
-           mp[i]++;
-       } 
-       for(auto i:t)
-       {
-           if(mp[i]>0)
-           {
-               mp[i]--;
-           }
-       }
-       int count=0;
-       for(auto i:mp)
-       {
-           count+=i.second;
-       }
-       return count;
+        unordered_map<char,int>mp;
+        int ans = 0;
+        for(int i = 0;i<s.length();i++){
+            mp[s[i]]++;
+        }
+        
+        for(int i = 0;i<t.length();i++){
+            if(mp[t[i]] > 0)mp[t[i]]--;
+        }
+        
+        for(auto it :mp){
+            ans += it.second;
+        }
+        
+        return ans;
+        
+        
     }
 };
