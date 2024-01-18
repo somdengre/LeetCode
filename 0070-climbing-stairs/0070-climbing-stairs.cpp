b6461dc4021b2b45df125b1897da783c9cb266a5
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-01-18 12:51:04 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (8.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
- public:
-  int climbStairs(int n) {
-    int p = 1;
-    int p1 = 1;
-      
-      for(int i = 2;i<= n ;i++){
-          int dp = p+p1;
-          p1 = p;
-          p = dp;
-      }
-      
-      return p;
-  }
+public:
+    int f(int n, vector<int>&dp){
+        if(n<0)return 0;
+        if(n == 0)return 1;
+        if(dp[n] != -1)return dp[n];
+        return dp[n] = f(n-1,dp) + f(n-2,dp); 
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
+    }
 };
