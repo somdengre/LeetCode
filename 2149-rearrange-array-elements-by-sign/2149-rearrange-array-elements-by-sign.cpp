d5e7809d1931540b4f8869f14927830776814de5
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-01-25 15:00:22 +0530

    Time: 157 ms (73.40%), Space: 126.2 MB (46.61%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        queue<int>q1,q2;
-        for(int i= 0;i<nums.size();i++){
-            if(nums[i]<0){
-                q2.push(nums[i]);
-            }else{
-                q1.push(nums[i]);
-            }
-        }
+//         queue<int>q1,q2;
+//         for(int i= 0;i<nums.size();i++){
+//             if(nums[i]<0){
+//                 q2.push(nums[i]);
+//             }else{
+//                 q1.push(nums[i]);
+//             }
+//         }
+//         for(int i = 0;i<nums.size();i++){
+//             if(i%2 == 0){
+//                 nums[i] = q1.front();
+//                 q1.pop();
+//             }else{
+//                 nums[i] = q2.front();
+//                 q2.pop();
+//             }
+//         }
+        
+//         return nums;
+        vector<int>ans(nums.size(),0);
+        int p = 0;
+        int n = 1;
         for(int i = 0;i<nums.size();i++){
-            if(i%2 == 0){
-                nums[i] = q1.front();
-                q1.pop();
+            if(nums[i] <0){
+                ans[n] = nums[i]; 
+                n+=2; 
             }else{
-                nums[i] = q2.front();
-                q2.pop();
+                ans[p] = nums[i];
+                p+=2;
             }
         }
         
-        return nums;
+        return ans;
         
     }
 };
