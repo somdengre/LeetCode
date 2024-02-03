c295cefe6e07b874cd8ab8d43178e4d9ea530192
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-02-03 15:09:15 +0530

    Time: 3 ms (99.46%), Space: 10.9 MB (23.07%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
-    int f(int ind,vector<int>&nums,int k,vector<int>&dp){
+    // int f(int ind,vector<int>&nums,int k,vector<int>&dp){
+    //     int n = nums.size();
+    //     if(ind == n)return 0;
+    //     if(dp[ind]!= -1)return dp[ind];
+    //     int maxans = INT_MIN;
+    //     int maxi = INT_MIN;
+    //     int l = 0;
+    //     // int n = nums.size()-1;
+    //     for(int j = ind;j < min(n,ind+k);j++){
+    //         l++;
+    //         maxi = max(maxi,nums[j]);
+    //         int sum = l*maxi+ f(j+1,nums,k,dp);
+    //         maxans = max(maxans,sum);
+    //     }
+    //     return dp[ind] = maxans;        
+    // }
+    int maxSumAfterPartitioning(vector<int>& nums, int k) {
         int n = nums.size();
-        if(ind == n)return 0;
-        if(dp[ind]!= -1)return dp[ind];
-        int maxans = INT_MIN;
-        int maxi = INT_MIN;
-        int l = 0;
+        vector<int>dp(n+1,0);
+        
+        for(int ind = n-1;ind>=0;ind--){
+             int maxans = INT_MIN;
+             int maxi = INT_MIN;
+             int l = 0;
         // int n = nums.size()-1;
-        for(int j = ind;j < min(n,ind+k);j++){
-            l++;
-            maxi = max(maxi,nums[j]);
-            int sum = l*maxi+ f(j+1,nums,k,dp);
-            maxans = max(maxans,sum);
+            for(int j = ind;j < min(n,ind+k);j++){
+                l++;
+                maxi = max(maxi,nums[j]);
+                int sum = l*maxi+ dp[j+1];
+                maxans = max(maxans,sum);
+            }
+            dp[ind] = maxans; 
         }
-        return dp[ind] = maxans;        
-    }
-    int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        int n = arr.size();
-        vector<int>dp(n,-1);
-        return f(0,arr,k,dp);
+        return dp[0];
     }
 };
