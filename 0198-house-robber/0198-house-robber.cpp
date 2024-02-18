58271a0c5516b36d7a5b6def1fa5911ba4fed67d
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-02-18 14:52:07 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (23.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,7 +15,17 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1,-1);
-        return f(n-1,nums,dp);
+        vector<int>dp(n+1,0);
+        dp[0] = nums[0];
+        for(int i = 1;i<n;i++){
+            int pick = nums[i];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            int npick = dp[i-1];
+        
+            dp[i] = max(pick,npick);
+        }
+        return dp[n-1];
     }
 };
