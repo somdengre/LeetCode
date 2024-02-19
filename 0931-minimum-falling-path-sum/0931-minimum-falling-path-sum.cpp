5b411a3b9c2f64b4aeffa629bd274d8691691385
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-02-19 16:14:43 +0530

    Time: 16 ms (18.22%), Space: 13.5 MB (7.70%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     
-    int f(vector<vector<int>>&nums,int i,int j,int n,vector<vector<int>>&dp){
-        if(i == 0 && j < nums.size() && j >= 0)return nums[i][j];
+//     int f(vector<vector<int>>&nums,int i,int j,int n,vector<vector<int>>&dp){
+//         if(i == 0 && j < nums.size() && j >= 0)return nums[i][j];
        
-        if(j<0 || j>=n)return 1e9;
-         if(dp[i][j] != INT_MAX)return dp[i][j];
-        int d = nums[i][j] + f(nums,i-1,j,n,dp);
-        int l =  nums[i][j] + f(nums,i-1,j-1,n,dp);
-        int r =  nums[i][j] + f(nums,i-1,j+1,n,dp);
+//         if(j<0 || j>=n)return 1e9;
+//          if(dp[i][j] != INT_MAX)return dp[i][j];
+//         int d = nums[i][j] + f(nums,i-1,j,n,dp);
+//         int l =  nums[i][j] + f(nums,i-1,j-1,n,dp);
+//         int r =  nums[i][j] + f(nums,i-1,j+1,n,dp);
         
-        return dp[i][j] = min(d,min(l,r));
-    }
+//         return dp[i][j] = min(d,min(l,r));
+//     }
     int minFallingPathSum(vector<vector<int>>& nums) {
         int mini = INT_MAX;
         int n= nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,INT_MAX));
-        
+        vector<vector<int>>dp(n,vector<int>(n,0));
         for(int j = 0;j<n;j++){
-            mini = min(f(nums,n-1,j,n,dp), mini);
+            dp[0][j] = nums[0][j];
+        }
+        
+        for(int i = 1;i<n;i++){
+            for(int j = 0;j<n;j++){
+                int d = nums[i][j] + dp[i-1][j];
+                int l = 1e9;
+                int r = 1e9;
+                if(j-1>=0) l =  nums[i][j] + dp[i-1][j-1];
+                if(j+1<n) r =  nums[i][j] + dp[i-1][j+1];
+        
+                dp[i][j] = min(d,min(l,r));
+            }
+        }
+       
+        for(int i = 0;i<n;i++){
+            mini = min(mini,dp[n-1][i]);
         }
         return mini;
     }
