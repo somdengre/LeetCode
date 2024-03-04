f797d5b05c9326448d07a648ef1117ef8cc1111b
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-03-04 15:15:02 +0530

    Time: 28 ms (72.23%), Space: 47.2 MB (21.98%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int f(int i,int t,vector<int>nums,vector<vector<int>>&dp){
+    int f(int i,vector<int>&nums,int target,vector<vector<int>>&dp){
         if(i == 0){
-            if(t%nums[i] == 0){
-                return t/nums[i];
-            }else{
-                return 1e9;
+            if(target%nums[i] == 0){
+                return target/nums[i];
+            }
+            else{
+                return 1e9; 
             }
         }
-        if(dp[i][t] != -2)return dp[i][t];
+        if(dp[i][target] != -2)return dp[i][target];
         
-        int npick = f(i-1,t,nums,dp);
-        int pick = INT_MAX;
-        if(t>=nums[i]){
-            pick = 1 + f(i,t-nums[i],nums,dp);
+        int np = f(i-1,nums,target,dp);
+        int p = INT_MAX;
+        if(target>=nums[i]){
+            p = 1+f(i,nums,target-nums[i],dp);
         }
-        
-        return dp[i][t] = min(pick,npick);
+        return dp[i][target] = min(p,np);
     }
-    int coinChange(vector<int>& nums, int t) {
+    int coinChange(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(t+1,-2));
-        int ans = f(n-1,t,nums,dp);
-        if(ans>= 1e9){
+        int ans = 0;
+        vector<vector<int>>dp(n,vector<int>(target+1,-2));
+        ans = f(n-1,nums,target,dp);
+        if(ans>=1e9){
             return -1;
         }
         return ans;
