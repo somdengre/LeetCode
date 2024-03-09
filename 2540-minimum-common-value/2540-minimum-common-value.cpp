3f39d69579e12d8e44b43983d5c0396e01cf1a31
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-03-09 14:33:12 +0530

    Time: 72 ms (54.08%), Space: 53 MB (29.59%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>mp;
+        int i = 0;
+        int j = 0;
         
-        for(int i = 0;i<nums2.size();i++){
-            mp[nums2[i]]++;
-        }
-        
-        
-        for(int i = 0;i<nums1.size();i++){
-            if(mp.find(nums1[i]) != mp.end()){
+        while(i!= nums1.size() && j!= nums2.size()){
+            if(nums1[i] == nums2[j]){
                 return nums1[i];
+            }else if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                i++;
             }
         }
         
