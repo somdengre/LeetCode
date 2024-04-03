2104625c83e47c4a6019f3eaaf5f2c8ff5e64eb7
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-04-03 14:40:59 +0530

    Time: 759 ms (49.38%), Space: 9.7 MB (74.33%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+     int dr1[4]={1,-1,0,0};
+    int dr2[4]={0,0,1,-1};
+public:
+    bool f(vector<vector<char>>& board,int& row,int& col,int i,int j,string& word,int ind,int n){
+        if(ind == n-1 && word[ind] == board[i][j]){
+            return true;
+        }
+        for(int d = 0;d<4;d++){
+            int x = i + dr1[d],y = j + dr2[d];
+            if(x>=0&&x<row&&y>=0&&y<col && board[x][y]!='.' && board[x][y]==word[ind]){
+                char ch = board[x][y];
+                board[x][y] = '.';
+                if(ind == n-1)return true;
+                if(f(board,row,col,x,y,word,ind+1,n) == true)return true;
+                board[x][y] = ch;
+            }
+        }
+        
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = word.length();
+        int row = board.size();
+        int col = board[0].size();
+        for(int i = 0;i<board.size();i++){
+            for(int j = 0;j<board[i].size();j++){
+                if(f(board,row,col,i,j,word,0,n)==true)return true;
+            }
+        }
+        return false;
+    }
+};
