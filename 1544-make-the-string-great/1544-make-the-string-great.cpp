45c5ff8c87665e65dd094e13d228d5db7e6b5cb6
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-04-05 23:02:20 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (24.89%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string makeGood(string s) {
-        string sb = s;
-        int flag = 0;
-        
-        while(flag == 0 && sb.length() >0){
-            flag = 1;
-            
-            for(int i = 0;i<sb.length()-1;i++){
-                if(abs(sb[i]-sb[i+1]) == 32){
-                    sb.erase(i,2);
-                    flag = 0;
-                    break;
-                }
-            }
+    stack<char> st;
+    string ans = "";
+    
+    for(int i=0; i<s.length(); i++) {
+        if(!st.empty() && abs(s[i]-st.top()) == 32) {
+            st.pop();
         }
-        
-        return sb;
+        else{
+            st.push(s[i]);
+        }
+        }
+          while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+          }
+    reverse(ans.begin(), ans.end());
+    return ans;
     }
 };
