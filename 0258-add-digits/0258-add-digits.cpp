73d288e603f1022f87fdcca7832898c8963c1fff
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-04-12 04:25:24 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (16.05%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int x = 0;
+        while(num>=10){
+            x = 0;
+            while(num){
+                x+=num%10;
+                num/=10;
+            }
+            num = x;
+        }
+        
+        return num;
+    }
+};
+
