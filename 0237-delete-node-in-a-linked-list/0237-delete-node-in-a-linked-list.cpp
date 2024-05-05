1a2285dca00c4fa173509b4989e040e35cbea4f7
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-05-05 13:03:47 +0530

    Time: 6 ms (53.82%), Space: 11.3 MB (84.10%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,14 +9,12 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        
         while(node){
             node->val = node->next->val;
-            if(node->next != NULL && node->next->next == NULL ){
+            if(node->next && node->next->next == NULL){
                 node->next = NULL;
             }
             node = node->next;
         }
-        
     }
 };
