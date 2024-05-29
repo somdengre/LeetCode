46ca279f50948f39c0e74bfce39349493e3fd86c
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-05-29 19:02:53 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (5.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    int f(int i,vector<int>&nums,vector<int>&dp){
-        if(i == 0){
-            return nums[0];
-        }
+    int f(int i,vector<int>nums,vector<int>&dp){
+        if(i == 0)return nums[0];
+        if(i == 1)return max(nums[0],nums[1]);
         if(dp[i] != -1)return dp[i];
-        int pick = nums[i];
-        if(i>1){
-            pick+=f(i-2,nums,dp);
+        int np = f(i-1,nums,dp);
+        int p = INT_MIN;
+        if(i>1){ 
+            p = nums[i] + f(i-2,nums,dp);
         }
-        int npick = f(i-1,nums,dp);
         
-        return dp[i] = max(pick,npick);
+        return dp[i] = max(p,np);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1,0);
-        dp[0] = nums[0];
-        for(int i = 1;i<n;i++){
-            int pick = nums[i];
-            if(i>1){
-                pick+=dp[i-2];
-            }
-            int npick = dp[i-1];
-        
-            dp[i] = max(pick,npick);
-        }
-        return dp[n-1];
+        vector<int>dp(n,-1);
+        return f(n-1,nums,dp);
     }
 };
