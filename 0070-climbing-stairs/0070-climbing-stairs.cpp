b2b46172efd6700cffd3436d38f408d6e9ca8da9
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-05-29 18:06:36 +0530

    Time: 3 ms (29.40%), Space: 7.5 MB (11.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int f(int n,vector<int>&dp){
+//     int f(int n,vector<int>&dp){
+//         if(n == 0)return 1;
+//         if(n == 1)return 1;
+        
+//         if(dp[n] != -1)return dp[n];
+//         int n1 = f(n-1,dp);
+//         int n2 = 0;
+//         if(n>1)n2 = f(n-2,dp);
+        
+//         return dp[n] = n1+n2;
+//     }
+    int climbStairs(int n) {
         if(n == 0)return 1;
         if(n == 1)return 1;
+        vector<int>dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
         
-        if(dp[n] != -1)return dp[n];
-        int n1 = f(n-1,dp);
-        int n2 = 0;
-        if(n>1)n2 = f(n-2,dp);
-        
-        return dp[n] = n1+n2;
-    }
-    int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return f(n,dp);
+        return dp[n];
     }
 };
