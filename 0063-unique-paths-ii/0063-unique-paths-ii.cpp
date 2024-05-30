8bff5d63c21d647c4121a762120ba2c666ff2a1a
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-05-31 01:24:49 +0530

    Time: 67 ms (19.53%), Space: 67.6 MB (8.19%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
-//     int f(int i,int j,vector<vector<int>>nums,vector<vector<int>>&dp){
-//         if(nums[i][j] == 1)return 0;
-//         if(i == 0 && j == 0){
-//             return 1;
-//         }
-//         if(i<0 || j<0)return 0;
+    int f(int i,int j,vector<vector<int>>nums,vector<vector<int>>&dp){
+        if(nums[i][j] == 1)return 0;
+        if(i<0 || j<0)return 0;
+        if(i == 0 && j == 0)return 1;
         
-//         if(dp[i][j] != -1)return dp[i][j];
-//         int left = 0;
-//         if(j>0)left = f(i,j-1,nums,dp);
-//         int up = 0;
-//         if(i>0)up = f(i-1,j,nums,dp);
-//         return dp[i][j] = up+left;
+        if(dp[i][j] != -1)return dp[i][j];
         
-//     }
+        int up = 0;
+        if(i>0)up = f(i-1,j,nums,dp);
+        int left = 0;
+        if(j>0)left = f(i,j-1,nums,dp);
+        
+        return dp[i][j] = up+left;
+        
+        
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& nums) {
-        int m = nums.size();
-        int n = nums[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[0][0] = 1;
-        // for(int p = 0;p<m;p++){
-        //     for(int q = 0;q<n;q++){
-        //         if(nums[p][q] == 1)dp[p][q] = 0;
-        //     }
-        // }
+        int n = nums.size();
+        int m = nums[0].size();
         
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(nums[i][j] == 1){
-                    dp[i][j] = 0;
-                    continue;
-                }
-                if(i == 0 && j == 0){
-                    dp[i][j] = 1;
-                    continue;
-                }
-               
-                int left = 0;
-                if(j>0)left = dp[i][j-1];
-                int up = 0;
-                if(i>0)up = dp[i-1][j];
-                dp[i][j] = up+left;
-                
-            }
-        }
-        return dp[m-1][n-1];
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        
+        return f(n-1,m-1,nums,dp);
     }
 };
