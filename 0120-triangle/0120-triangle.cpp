5701770e6f864fe2d5ae6c2c066d67dce934046e
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-05-31 02:50:15 +0530

    Time: 4 ms (72.91%), Space: 11.4 MB (53.88%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    int f(int i,int j,vector<vector<int>>&nums,vector<vector<int>>&dp){
-        if(i >= nums.size() || j>= nums[i].size())return 1e9;
-        if(i == nums.size()-1)return nums[i][j];
-        if(dp[i][j] != -1e9)return dp[i][j];
+//     int f(int i,int j,vector<vector<int>>&nums,vector<vector<int>>&dp){
+//         if(i >= nums.size() || j>= nums[i].size())return 1e9;
+//         if(i == nums.size()-1)return nums[i][j];
+//         if(dp[i][j] != -1e9)return dp[i][j];
         
-        int down = nums[i][j] + f(i+1,j,nums,dp);
-        int right = nums[i][j] + f(i+1,j+1,nums,dp);
+//         int down = nums[i][j] + f(i+1,j,nums,dp);
+//         int right = nums[i][j] + f(i+1,j+1,nums,dp);
         
-        return dp[i][j] = min(down,right);
-    }
+//         return dp[i][j] = min(down,right);
+//     }
     int minimumTotal(vector<vector<int>>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1e9));
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i = 0;i<nums[n-1].size();i++){
+            dp[n-1][i] = nums[n-1][i];
+        }
         
-        return f(0,0,nums,dp);
+        for(int i = n-1;i>=0;i--){
+            for(int j = i;j>=0;j--){
+                int down = nums[i][j] + dp[i+1][j];
+                int right = nums[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down,right);
+            }
+        }
+        return dp[0][0];
     }
 };
