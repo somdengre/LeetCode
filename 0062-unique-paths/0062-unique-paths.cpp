d94bb700b6db1f82856dfba1894759b2edb87298
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-05-31 01:07:46 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (44.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int f(int i,int j,vector<vector<int>>&dp){
-        if(j<0){
-            return 0;
-        }
-        if(i<0)return 0;
+        if(i<0 || j<0)return 0;
         if(i == 0 && j == 0){
             return 1;
         }
+        
         if(dp[i][j] != -1)return dp[i][j];
+        int up = 0;
+        up = f(i-1,j,dp);
+        int left = 0;
+        left = f(i,j-1,dp);
         
-        int up = f(i-1,j,dp);
-        int left = f(i,j-1,dp);
-        return dp[i][j] = up+left;
-        
+        return dp[i][j] =  up+left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp(m,vector<int>(n,-1));
         return f(m-1,n-1,dp);
     }
 };
