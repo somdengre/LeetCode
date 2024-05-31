8df08b85fe91e5d60d1b7fe2ab28e0d9ae689c89
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-01 02:20:07 +0530

    Time: 9 ms (66.24%), Space: 13.7 MB (55.02%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int f(int i,int k,vector<int>&nums,vector<vector<int>>&dp){
-        if(i == 0){
-            if(k == 0 && nums[i] == 0){
-                return 2;
-            }else if(k == nums[i]|| k == 0  ){
-                return 1;
-            }else{
-                return 0;
-            }
-        }
+//     int f(int i,int k,vector<int>&nums,vector<vector<int>>&dp){
+//         if(i == 0){
+//             if(k == 0 && nums[i] == 0){
+//                 return 2;
+//             }else if(k == nums[i]|| k == 0  ){
+//                 return 1;
+//             }else{
+//                 return 0;
+//             }
+//         }
         
-        if(dp[i][k] != -1)return dp[i][k];
+//         if(dp[i][k] != -1)return dp[i][k];
         
-        int np = f(i-1,k,nums,dp);
-        int p = 0;
-        if(nums[i]<=k)p = f(i-1,k-nums[i],nums,dp);
+//         int np = f(i-1,k,nums,dp);
+//         int p = 0;
+//         if(nums[i]<=k)p = f(i-1,k-nums[i],nums,dp);
         
-        return p+np;
-    }
+//         return p+np;
+//     }
     int findTargetSumWays(vector<int>& nums, int d) {
         int n = nums.size();
         int s = 0;
@@ -30,6 +30,25 @@ public:
         
         k/=2;
         vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        return f(n-1,k,nums,dp);
+        for(int i = 0;i<=k;i++){
+            if(i == 0 && nums[0] == 0){
+                dp[0][i] = 2;
+            }else if(i == nums[0] || i == 0){
+                dp[0][i] = 1;
+            }else{
+                dp[0][i] = 0;
+            }
+        }
+        
+        for(int i = 1;i<n;i++){
+            for(int j = 0;j<=k;j++){
+                 int np = dp[i-1][j];
+                 int p = 0;
+                 if(nums[i]<=j)p = dp[i-1][j-nums[i]];
+        
+                 dp[i][j] = p+np;
+            }
+        }
+        return dp[n-1][k];
     }
 };
