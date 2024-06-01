37292a6a317430242e1532f5fc21977f25068c82
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-02 01:30:04 +0530

    Time: 35 ms (34.33%), Space: 29.5 MB (64.31%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    int f(string text1, string text2) {
-        int n = text1.length();
-        int m = text2.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        
+    // int f(int i,int j,string s,string t,vector<vector<int>>&dp){
+    //     if(i == 0 || j == 0){
+    //         return 0;
+    //     }
+    //     if(dp[i][j] != -1)return dp[i][j];
+    //     if(s[i-1] == t[i-1]){
+    //         return dp[i][j] =  1 + f(i-1,j-1,s,t,dp);
+    //     }else{
+    //         return dp[i][j] = max(f(i-1,j,s,t,dp),f(i,j-1,s,t,dp));
+    //     }
+    // }
+    int minInsertions(string s) {
+        string t = s;
+        reverse(t.begin(),t.end());
+        int n = s.length();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         for(int i = 0;i<=n;i++){
             dp[i][0] = 0;
+            dp[0][i] = 0;
         }
-        for(int j = 0;j<=m;j++){
-            dp[0][j] = 0;
-        }
-        
         for(int i = 1;i<=n;i++){
-            for(int j = 1;j<=m;j++){
-                if(text1[i-1] == text2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-            
+            for(int j = 1;j<=n;j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
-                    dp[i][j] = 0 + max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1] );
                 }
-        
-                
-                
             }
         }
         
-        
-        return dp[n][m];
-
-    }
-    int longestPalindromeSubseq(string s) {
-        std::string t = s;
-        std::reverse(t.begin(), t.end()); 
-        return f(s, t);
-    }
-
-    int minInsertions(string s) {
-        int n =s.length();
-        return n -  longestPalindromeSubseq(s);
+        return n - dp[n][n];
     }
 };
