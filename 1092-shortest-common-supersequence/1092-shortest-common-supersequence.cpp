990446dd64af7191abe8af644c5994244816c3db
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-02 02:14:44 +0530

    Time: 26 ms (35.90%), Space: 25.6 MB (67.26%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,48 +1,52 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string x, string y) {
-        int n = x.length();
-        int m = y.length();
-        vector<vector<int>>dp(n+1, vector<int> (m+1,0));
+    string shortestCommonSupersequence(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         
+        for(int i = 0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        for(int j = 0;j<=m;j++){
+            dp[0][j] = 0;
+        }
         for(int i = 1;i<=n;i++){
-            for(int j= 1;j<=m;j++){
-                if(x[i-1] == y[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+            for(int j = 1;j<=m;j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
                 }else{
-                    dp[i][j] = 0 + max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
         
-        int i = n;
-        int j=m;
         string ans = "";
+        int i = n;
+        int j = m;
         while(i>0 && j>0){
-            if(x[i-1] == y[j-1]){
-                ans+=x[i-1];
-                i--;j--;
+            if(s[i-1] == t[j-1]){
+                ans+=s[i-1];
+                i--;
+                j--;
             }else if(dp[i-1][j] > dp[i][j-1]){
-                ans+=x[i-1];
+                ans+=s[i-1];
                 i--;
             }else{
-                ans+=y[j-1];
+                ans+=t[j-1];
                 j--;
             }
         }
-        
         while(i>0){
-            ans+=x[i-1];
+            ans+=s[i-1];
             i--;
         }
+        
         while(j>0){
-            ans+=y[j-1];
+            ans+=t[j-1];
             j--;
         }
-        
         reverse(ans.begin(),ans.end());
         return ans;
-        
-        
     }
 };
