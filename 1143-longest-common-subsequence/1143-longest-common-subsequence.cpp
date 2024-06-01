2fc2fb9e306bbd6acf96f2653fa03687965cd860
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-01 23:29:14 +0530

    Time: 27 ms (48.21%), Space: 25.6 MB (51.42%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-//     int f(int i,int j,string s1,string s2,vector<vector<int>>&dp){
-//         if(i == 0 || j == 0)return 0;
-//         if(dp[i][j] != -1)return dp[i][j];
-//         if(s1[i-1] == s2[j-1]){
-//             return dp[i][j] = 1+f(i-1,j-1,s1,s2,dp);
-            
-//         }
-        
-//         return dp[i][j] = 0 + max(f(i-1,j,s1,s2,dp), f(i,j-1,s1,s2,dp));
-//     }
+    int f(int i,int j,string s1,string s2,vector<vector<int>>&dp){
+        if(i == 0 || j == 0){
+            return 0;
+        }
+        if(dp[i][j] != -1)return dp[i][j];
+        if(s1[i-1] == s2[j-1]){
+            return dp[i][j] = 1 + f(i-1,j-1,s1,s2,dp);
+        }else{
+            return dp[i][j] =  max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
+        }
+    }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length();
         int m = text2.length();
@@ -25,19 +26,12 @@ public:
         for(int i = 1;i<=n;i++){
             for(int j = 1;j<=m;j++){
                 if(text1[i-1] == text2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-            
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
-                    dp[i][j] = 0 + max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] =  max(dp[i-1][j],dp[i][j-1]);
                 }
-        
-                
-                
             }
         }
-        
-        
         return dp[n][m];
-
     }
 };
