c44a18904148dff7fedf045b06ca5849ff53756e
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-03 00:10:09 +0530

    Time: 19 ms (92.06%), Space: 30.8 MB (35.43%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,32 +1,55 @@
 class Solution {
 public:
-    bool solve(int i,int j,string &s,string &t,vector<vector<int>> &dp)
-    {
-        if(i==0 && j==0)
-            return true;
-        if(i==0 && j>0)
-            return false;
-        if(i>0 && j==0)
-        {
-            for(int k=0;k<i;k++)
-            {
-                if(s[k]!='*')
-                    return false;
-            }
-            return true;
+    // bool f(int i,int j ,string s,string p,vector<vector<int>>&dp){
+    //     if(i==0 && j==0)return true;
+    //     if(i==0 && j>0)return false;
+    //     if(i>0 && j == 0){
+    //         for(int ii = 1;ii<=i;ii++){
+    //             if(s[ii-1] != '*'){
+    //                 return false;
+    //             }
+    //         }
+    //         return true;
+    //     }
+    //     if(dp[i][j] !=-1)return dp[i][j];
+    //     if(s[i-1] == p[j-1] || s[i-1] == '?'){
+    //         return dp[i][j] = f(i-1,j-1,s,p,dp);
+    //     }
+    //     if(s[i-1] == '*'){
+    //         return dp[i][j] = f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp);
+    //     }
+    //     return false;
+    // }
+    bool isMatch(string p, string s) {
+        int n = s.length();
+        int m = p.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        dp[0][0] = true;
+        for(int j = 1;j<=m;j++){
+            dp[0][j] = false;
         }
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(s[i-1]==t[j-1] || s[i-1]=='?')
-            return dp[i][j]=solve(i-1,j-1,s,t,dp);
-        if(s[i-1]=='*')
-            return dp[i][j]=solve(i,j-1,s,t,dp) | solve(i-1,j,s,t,dp);
-        return dp[i][j]=false;
-    }
-    bool isMatch(string s, string t) {
-        int n=s.length();
-        int m=t.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return solve(m,n,t,s,dp);
+        for(int i = 1;i<=n;i++){
+            int fl = true;
+            for(int ii = 1;ii<=i;ii++){
+                if(s[ii-1] != '*'){
+                    fl = false;
+                    break;
+                }
+            }
+            dp[i][0] = fl;
+        }
+        
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(s[i-1] == p[j-1] || s[i-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(s[i-1] == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
     }
 };
