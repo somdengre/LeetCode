04b714efc06766a57f94bf57721f54a279175774
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-03 02:08:20 +0530

    Time: 414 ms (47.09%), Space: 209.5 MB (42.25%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
-    int f(int i,int buy,int cap,vector<int>&nums,vector<vector<vector<int>>>&dp){
-        if(cap == 0)return 0;
-        if(i == nums.size())return 0;
-        if(dp[i][buy][cap] != -1)return dp[i][buy][cap];
+//     int f(int i,int buy,int cap,vector<int>&nums,vector<vector<vector<int>>>&dp){
+//         if(cap == 0)return 0;
+//         if(i == nums.size())return 0;
+//         if(dp[i][buy][cap] != -1)return dp[i][buy][cap];
         
-        if(buy){
-            return dp[i][buy][cap] = max(-nums[i] + f(i+1,0,cap,nums,dp), 0 + f(i+1,1,cap,nums,dp));
-        }else{
-            return dp[i][buy][cap] = max(nums[i] + f(i+1,1,cap-1,nums,dp), 0 + f(i+1,0,cap,nums,dp));
-        }
-    }
+//         if(buy){
+//             return dp[i][buy][cap] = max(-nums[i] + f(i+1,0,cap,nums,dp), 0 + f(i+1,1,cap,nums,dp));
+//         }else{
+//             return dp[i][buy][cap] = max(nums[i] + f(i+1,1,cap-1,nums,dp), 0 + f(i+1,0,cap,nums,dp));
+//         }
+//     }
     int maxProfit(vector<int>& nums) {
         
         int n  = nums.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
         
-        return f(0,1,2,nums,dp);
+        for(int i = 0;i<=n;i++){
+            for(int buy = 0;buy<=1;buy++){
+                dp[i][buy][0] = 0;
+            }
+        }
+        
+        for(int cap = 0;cap<=2;cap++){
+            for(int buy = 0;buy<=1;buy++){
+                dp[n-1][buy][cap] = 0;
+            }
+        }
+        
+        for(int i = n-1;i>=0;i--){
+            for(int buy = 0;buy<=1;buy++){
+                for(int cap = 1;cap<=2;cap++){
+                    if(buy){
+                        dp[i][buy][cap] = max(-nums[i] + dp[i+1][0][cap], dp[i+1][1][cap]);
+                    }else{
+                        dp[i][buy][cap] = max(nums[i] + dp[i+1][1][cap-1] , dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
