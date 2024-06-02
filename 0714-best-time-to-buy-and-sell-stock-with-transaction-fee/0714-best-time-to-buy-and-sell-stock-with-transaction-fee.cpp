900015f7f08dcf49d6a0b23a69670d60c5ef9673
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-03 02:39:49 +0530

    Time: 176 ms (9.59%), Space: 98.6 MB (17.60%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
+    int f(int i,int buy,int fee,vector<int>&nums,vector<vector<int>>&dp){
+        if(i == nums.size())return 0;
+        if(dp[i][buy] != -1)return dp[i][buy];
         
-        vector<vector<int>>dp(n+1, vector<int>(2,0));
-        
-        for(int i = n-1; i>=0;i--){
-            for(int buy = 0;buy<=1;buy++){
-                if(buy){
-                    dp[i][buy] = max((-prices[i]-fee+dp[i+1][0]), dp[i+1][1]);
-                }else{
-                    dp[i][buy] = max((prices[i]+dp[i+1][1]),dp[i+1][0]);
-                }
-            }
+        if(buy){
+            return dp[i][buy] = max(-nums[i] + f(i+1,0,fee,nums,dp),f(i+1,1,fee,nums,dp));
+        }else{
+            return dp[i][buy] = max(nums[i]-fee+f(i+1,1,fee,nums,dp),f(i+1,0,fee,nums,dp));
         }
+    }
+    int maxProfit(vector<int>& nums, int fee) {
+        int n = nums.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
         
-        return dp[0][1];
+        return f(0,1,fee,nums,dp);
     }
 };
