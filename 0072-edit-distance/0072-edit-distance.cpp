f2ae309258789be0b8381ec7649cb53f0372c4a9
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-02 22:56:01 +0530

    Time: 51 ms (13.53%), Space: 82.5 MB (5.45%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int f(int i,int j,string s,string t,vector<vector<int>>&dp){
-        if(i<0)return j+1;
-        if(j<0)return i+1;
+        if(i==0)return j;
+        if(j==0)return i;
         
         if(dp[i][j] != -1)return dp[i][j];
         
-        if(s[i] == t[j]){
+        if(s[i-1] == t[j-1]){
             return dp[i][j] =  f(i-1,j-1,s,t,dp);    
         }else{
             return dp[i][j] = 1+min(f(i-1,j,s,t,dp),min( f(i,j-1,s,t,dp), f(i-1,j-1,s,t,dp)));
@@ -17,8 +17,8 @@ public:
         int n = s.length();
         int m = t.length();
         
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         
-        return f(n-1,m-1,s,t,dp);
+        return f(n,m,s,t,dp);
     }
 };
