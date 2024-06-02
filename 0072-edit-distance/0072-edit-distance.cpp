a9f22984a7507d55b9ac0bfff6135066274d89fe
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-02 22:59:27 +0530

    Time: 8 ms (55.17%), Space: 12.1 MB (24.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    int f(int i,int j,string s,string t,vector<vector<int>>&dp){
-        if(i==0)return j;
-        if(j==0)return i;
+//     int f(int i,int j,string s,string t,vector<vector<int>>&dp){
+//         if(i==0)return j;
+//         if(j==0)return i;
         
-        if(dp[i][j] != -1)return dp[i][j];
+//         if(dp[i][j] != -1)return dp[i][j];
         
-        if(s[i-1] == t[j-1]){
-            return dp[i][j] =  f(i-1,j-1,s,t,dp);    
-        }else{
-            return dp[i][j] = 1+min(f(i-1,j,s,t,dp),min( f(i,j-1,s,t,dp), f(i-1,j-1,s,t,dp)));
-        }
+//         if(s[i-1] == t[j-1]){
+//             return dp[i][j] =  f(i-1,j-1,s,t,dp);    
+//         }else{
+//             return dp[i][j] = 1+min(f(i-1,j,s,t,dp),min( f(i,j-1,s,t,dp), f(i-1,j-1,s,t,dp)));
+//         }
         
-    }
+//     }
     int minDistance(string s, string t) {
         int n = s.length();
         int m = t.length();
         
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i = 0;i<=n;i++){
+            dp[i][0] = i;
+        }
+        for(int j = 0;j<=m;j++){
+            dp[0][j] = j;
+        }
         
-        return f(n,m,s,t,dp);
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] =  dp[i-1][j-1];    
+                }else{
+                    dp[i][j] = 1+min(dp[i-1][j],min( dp[i][j-1], dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
