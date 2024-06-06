a86abe8e62a58036ca398230fe3149790744e6e7
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-07 02:06:28 +0530

    Time: 1498 ms (43.25%), Space: 7.5 MB (99.19%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -10,24 +10,23 @@ public:
         }
         return true;
     }
-    int f(int i,int n,string &s,vector<int>&dp){
-       
-        if(i == n)return 0;
-        if(dp[i] != -1)return dp[i];
-        int mini = INT_MAX;
-        
-        for(int j = i;j<n;j++){
-            if(check(i,j,s)){
-                int cost = 1 + f(j+1,n,s,dp);
-                mini = min(mini,cost);
-            }
-        }
-        
-        return dp[i]= mini;
-    }
+
     int minCut(string s) {
         int n = s.length();
-        vector<int>dp(n,-1);
-        return f(0,n,s,dp)-1;
+        vector<int>dp(n+1,0);
+        dp[n] = 0;
+        for(int i = n-1;i>=0;i--){
+            int mini = INT_MAX;
+        
+            for(int j = i;j<n;j++){
+                if(check(i,j,s)){
+                    int cost = 1 + dp[j+1];
+                    mini = min(mini,cost);
+                }
+            }
+        
+        dp[i]= mini;
+        }
+        return dp[0]-1;
     }
 };
