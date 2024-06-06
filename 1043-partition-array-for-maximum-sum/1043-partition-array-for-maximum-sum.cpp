50b88c957689627947b4848dc95b4672b3a62509
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-07 02:27:39 +0530

    Time: 10 ms (54.81%), Space: 10.9 MB (33.50%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-    // int f(int ind,vector<int>&nums,int k,vector<int>&dp){
-    //     int n = nums.size();
-    //     if(ind == n)return 0;
-    //     if(dp[ind]!= -1)return dp[ind];
-    //     int maxans = INT_MIN;
-    //     int maxi = INT_MIN;
-    //     int l = 0;
-    //     // int n = nums.size()-1;
-    //     for(int j = ind;j < min(n,ind+k);j++){
-    //         l++;
-    //         maxi = max(maxi,nums[j]);
-    //         int sum = l*maxi+ f(j+1,nums,k,dp);
-    //         maxans = max(maxans,sum);
-    //     }
-    //     return dp[ind] = maxans;        
-    // }
+    int f(int ind,int n,vector<int>&nums,int k,vector<int>&dp){
+        if(ind == n)return 0;
+        if(dp[ind] != -1)return dp[ind];
+        int maxAns = INT_MIN;
+        int maxi = INT_MIN;
+        int len = 0;
+        for(int j = ind;j<min(n,ind+k);j++){
+            len++;
+            maxi = max(maxi,nums[j]);
+            int sum = len*maxi + f(j+1,n,nums,k,dp);
+            maxAns = max(maxAns,sum);
+        }
+        
+        return dp[ind] = maxAns;
+    }
     int maxSumAfterPartitioning(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int>dp(n+1,0);
+        vector<int>dp(n,-1);
         
-        for(int ind = n-1;ind>=0;ind--){
-             int maxans = INT_MIN;
-             int maxi = INT_MIN;
-             int l = 0;
-        // int n = nums.size()-1;
-            for(int j = ind;j < min(n,ind+k);j++){
-                l++;
-                maxi = max(maxi,nums[j]);
-                int sum = l*maxi+ dp[j+1];
-                maxans = max(maxans,sum);
-            }
-            dp[ind] = maxans; 
-        }
-        return dp[0];
+        return f(0,n,nums,k,dp);
     }
 };
