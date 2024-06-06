cb542459bb9604bffb5c62de5ce430fbbdba0d41
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-07 02:30:16 +0530

    Time: 6 ms (84.61%), Space: 11 MB (33.50%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    int f(int ind,int n,vector<int>&nums,int k,vector<int>&dp){
-        if(ind == n)return 0;
-        if(dp[ind] != -1)return dp[ind];
-        int maxAns = INT_MIN;
-        int maxi = INT_MIN;
-        int len = 0;
-        for(int j = ind;j<min(n,ind+k);j++){
-            len++;
-            maxi = max(maxi,nums[j]);
-            int sum = len*maxi + f(j+1,n,nums,k,dp);
-            maxAns = max(maxAns,sum);
-        }
+//     int f(int ind,int n,vector<int>&nums,int k,vector<int>&dp){
+//         if(ind == n)return 0;
+//         if(dp[ind] != -1)return dp[ind];
+//         int maxAns = INT_MIN;
+//         int maxi = INT_MIN;
+//         int len = 0;
+//         for(int j = ind;j<min(n,ind+k);j++){
+//             len++;
+//             maxi = max(maxi,nums[j]);
+//             int sum = len*maxi + f(j+1,n,nums,k,dp);
+//             maxAns = max(maxAns,sum);
+//         }
         
-        return dp[ind] = maxAns;
-    }
+//         return dp[ind] = maxAns;
+//     }
     int maxSumAfterPartitioning(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int>dp(n,-1);
+        vector<int>dp(n+1,0);
+        dp[n] = 0;
         
-        return f(0,n,nums,k,dp);
+        for(int ind = n-1;ind>=0;ind--){
+            int maxAns = INT_MIN;
+            int maxi = INT_MIN;
+            int len = 0;
+            for(int j = ind;j<min(n,ind+k);j++){
+                len++;
+                maxi = max(maxi,nums[j]);
+                int sum = len*maxi + dp[j+1];
+                maxAns = max(maxAns,sum);
+            }
+        
+            dp[ind] = maxAns;
+        }
+        
+        return dp[0];
     }
 };
