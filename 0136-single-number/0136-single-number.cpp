2a61428d0df313459c5f4df0d4a8cfa92a36c831
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-07 22:26:29 +0530

    Time: 11 ms (81.72%), Space: 19.3 MB (30.49%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int>mp;
+        int ans = 0;
         
         for(int i = 0;i<nums.size();i++){
-            mp[nums[i]]++;
+            ans^=nums[i];
         }
-        int ans = 0;
-        for(auto it:mp){
-            if(it.second == 1){
-                ans = it.first;
-            }
-        }
+        
         return ans;
     }
 };
