69f2b234cf7b6416506494f254213e9b1afb4f3d
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-07 23:29:33 +0530

    Time: 153 ms (9.68%), Space: 12.7 MB (74.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
+    vector<int> twoSum(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int>ans;
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                if(nums[i] + nums[j] == k){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
                 }
             }
         }
-        return {};
+        
+        return ans;
     }
 };
