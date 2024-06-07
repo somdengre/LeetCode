d38f03e5dff1c82bdfef05dfc9c8448ca68d7d17
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-08 00:38:11 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (71.62%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        map<int,int>mp;
-        for(int i =0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        int i = 0;
-        for(auto it: mp){
-            int t = it.second;
-            while(t){
-                nums[i] = it.first;
-                i++;
-                t--;
+        int zero = 0;
+        int one  = 0;
+        int two = 0;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            if(nums[i] == 0){
+                zero++;
+            }else if(nums[i] == 1){
+                one++;
+            }else{
+                two++;
             }
         }
         
+        for(int i = 0;i<zero;i++){
+            nums[i] = 0;
+        }
+        for(int i = zero;i<zero+one;i++){
+            nums[i] = 1;
+        }
+        for(int i = zero+one;i<n;i++){
+            nums[i] = 2;
+        }
         
     }
 };
