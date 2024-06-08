97e0845a1af44b088041e7ad97a2911606e74ea2
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-08 23:20:30 +0530

    Time: 3 ms (37.77%), Space: 8.1 MB (52.01%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> spiralOrder(vector<vector<int>>& nums) {
         vector<int>ans;
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int n = nums.size();
+        int m = nums[0].size();
+        int top = 0;
         int left = 0;
         int right = m-1;
-        int top = 0;
         int bottom = n-1;
-        while(top<=bottom && left<=right){
-             for(int i = left;i<=right;i++){
-            ans.push_back(matrix[top][i]);
-        }
-        top++;
         
-        for(int i = top;i<=bottom;i++){
-            ans.push_back(matrix[i][right]);
-        }
-        right--;
-        if(top<=bottom){
+        
+        while(left<=right && top<=bottom){
+            for(int i = left;i<=right;i++){
+                ans.push_back(nums[top][i]);
+            }
+            top++;
+            for(int i = top;i<=bottom;i++){
+                ans.push_back(nums[i][right]);
+            }
+            right--;
+            
+            if(top<=bottom){
                 for(int i = right;i>=left;i--){
-            ans.push_back(matrix[bottom][i]);
-        }
+                    ans.push_back(nums[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i = bottom;i>=top;i--){
+                    ans.push_back(nums[i][left]);
+                }
+                left++;
+            }
+            
         }
         
-        bottom--;
-        if(left<=right){
-            for(int i = bottom;i>=top;i--){
-            ans.push_back(matrix[i][left]);
-        }
-        left++;
-        }
-        
-        
-        }
         return ans;
-       
         
     }
 };
