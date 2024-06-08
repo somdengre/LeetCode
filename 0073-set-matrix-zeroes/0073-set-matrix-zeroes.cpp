1b15e63b28db806f3627f2764aae707685cacb72
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-08 21:19:44 +0530

    Time: 10 ms (54.31%), Space: 16.4 MB (50.90%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,23 +4,39 @@ public:
         unordered_set<int>row,col;
         int n = nums.size();
         int m = nums[0].size();
-        
+        int col0 = 1;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(nums[i][j] == 0){
-                    row.insert(i);
-                    col.insert(j);
+                    nums[i][0] = 0;
+                    if(j!= 0){
+                        nums[0][j] = 0;
+                    }else{
+                        col0 = 0;
+                    }
                 }
             }
         }
         
-        
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(row.find(i) != row.end() || col.find(j) != col.end()){
+        for(int i = 1;i<n;i++){
+            for(int j = 1;j<m;j++){
+                if(nums[i][0] == 0 || nums[0][j] == 0){
                     nums[i][j] = 0;
                 }
             }
         }
+        
+        if(nums[0][0] == 0){
+            for(int j = 0;j<m;j++){
+                nums[0][j] = 0;
+            }
+        }
+        
+        if(col0 == 0){
+            for(int i = 0;i<n;i++){
+                nums[i][0] = 0;
+            }
+        }
+        
     }
 };
