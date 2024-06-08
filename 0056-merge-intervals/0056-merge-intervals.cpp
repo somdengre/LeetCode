d161d0e0a6e609fdef941c768eb57970f5058f32
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-09 04:20:26 +0530

    Time: 24 ms (50.55%), Space: 22.7 MB (68.31%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,21 +6,11 @@ public:
         vector<vector<int>>ans;
         
         for(int i = 0;i<n;i++){
-            int start = nums[i][0];
-            int end = nums[i][1];
-            
-            if(!ans.empty() && ans.back()[1] >= end){
-                continue;
+            if(ans.empty() || nums[i][0] > ans.back()[1]){
+                ans.push_back(nums[i]);
+            }else{
+                ans.back()[1] = max(ans.back()[1],nums[i][1]);
             }
-            
-            for(int j = i+1;j<n;j++){
-                if(nums[j][0] <= end){
-                    end = max(end,nums[j][1]);
-                }else{
-                    break;
-                }
-            }
-            ans.push_back({start,end});
         }
         
         return ans;
