029d4b637f3bde1bd6562a9c927377062c8d8a89
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-08 20:44:06 +0530

    Time: 7 ms (83.22%), Space: 17 MB (6.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& m) {
-        vector<int>row(m.size(),-1),col(m[0].size(),-1);
-        for(int i = 0;i<m.size();i++){
-            for(int j = 0;j<m[0].size();j++){
-                if(m[i][j] == 0){
-                    row[i] = 0;
-                    col[j] = 0;
+    void setZeroes(vector<vector<int>>& nums) {
+        unordered_set<int>row,col;
+        int n = nums.size();
+        int m = nums[0].size();
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(nums[i][j] == 0){
+                    row.insert(i);
+                    col.insert(j);
                 }
             }
         }
         
-        for(int i = 0;i<row.size();i++){
-            if(row[i] == 0){
-                for(int j = 0;j<m[i].size();j++){
-                    m[i][j] = 0; 
-                }
-            }
-        }
-        for(int i = 0;i<col.size();i++){
-            if(col[i] == 0){
-                for(int j = 0;j<m.size();j++){
-                    m[j][i] = 0; 
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(row.find(i) != row.end() || col.find(j) != col.end()){
+                    nums[i][j] = 0;
                 }
             }
         }
