f74973e171644845079bc8a32f6173edd6052454
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-11 04:36:34 +0530

    Time: 15 ms (84.71%), Space: 24.6 MB (85.62%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,49 +2,26 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        if(n==1)return nums[0];
-        if(nums[0]!=nums[1])return nums[0];
+        if(n == 1)return nums[0];
+        if(nums[0] != nums[1])return nums[0];
         if(nums[n-1]!= nums[n-2])return nums[n-1];
+        int low = 1;
+        int high = n-2;
         
-        int s = 1;
-        int e = n-2;
-        
-        while(s<=e){
-            int mid = s+(e-s)/2;
+        while(low<=high){
+            int mid = (low+high)/2;
             
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])return nums[mid];
+            if(nums[mid] != nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
             
-            else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
-                s = mid+1;
+            if((mid %2 == 1 && nums[mid-1] == nums[mid]) || (mid%2 == 0 && nums[mid+1] == nums[mid])){
+                low = mid+1;
             }else{
-                e = mid-1;
+                high = mid-1;
             }
         }
         
         return -1;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     int singleNonDuplicate(vector<int>& nums) {
-//       int n=nums.size();
-//         if(n==1)return nums[0];
-//         if(nums[0]!=nums[1])return nums[0];
-//         if(nums[n-1]!=nums[n-2])return nums[n-1];
-//         int st=1,end=n-2;
-//         while(st<=end){
-//             int mid=(st+end)/2;
-//             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])return nums[mid];
-
-//             if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]) ){
-//                 st=mid+1;
-//             }else{
-//                 end=mid-1;
-//             }
-//         }
-//         return -1;
-//     }
-// };
