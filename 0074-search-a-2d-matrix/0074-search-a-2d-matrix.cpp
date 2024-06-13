a49d0b1a262da6904f8d87061a42cd7a9c51dd08
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-13 20:02:51 +0530

    Time: 3 ms (66.33%), Space: 11.9 MB (48.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    bool bs(vector<int>&nums,int k,int m){
+    bool searchMatrix(vector<vector<int>>& nums, int k) {
+        int n = nums.size();
+        int m = nums[0].size();
         int low = 0;
-        int high = m-1;
+        int high = m*n-1;
         
         while(low<=high){
             int mid = (low+high)/2;
-            if(nums[mid] == k){
+            int row = mid/m;
+            int col = mid%m;
+            if(nums[row][col] == k){
                 return true;
-            }else if(nums[mid] > k){
+            }else if(nums[row][col]> k){
                 high = mid-1;
             }else {
                 low = mid+1;
             }
         }
         return false;
-    }
-    bool searchMatrix(vector<vector<int>>& nums, int k) {
         
-        int n = nums.size();
-        int m = nums[0].size();
-        
-        for(int i = 0;i<n;i++){
-            if(nums[i][0] <= k && nums[i][m-1] >=k){
-                return bs(nums[i],k,m);
-            }
-        }
-        
-        return false;
     }
 };
