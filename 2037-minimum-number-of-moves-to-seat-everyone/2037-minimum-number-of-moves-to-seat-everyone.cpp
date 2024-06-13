93a1a0e5413607c4379ed0f67b2dabd91d7607f5
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-13 19:20:26 +0530

    Time: 0 ms (100.00%), Space: 22.5 MB (17.35%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -3,11 +3,20 @@ public:
     int minMovesToSeat(vector<int>& seats, vector<int>& nums) {
         int n =nums.size();
         int ans = 0;
+        priority_queue<int> pq , student;
         
-        sort(seats.begin(),seats.end());
-        sort(nums.begin(), nums.end());
         for(int i = 0;i<n;i++){
-            ans+=abs(nums[i] - seats[i]);
+            pq.push(seats[i]);
+            student.push(nums[i]);
+        }
+        
+        while(!pq.empty()){
+            int a = pq.top();
+            int b = student.top();
+            pq.pop();
+            student.pop();
+            
+            ans+=abs(a-b);
         }
         
         return ans;
