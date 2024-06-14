40f79f723b113fe0fcbe1d909f6f94cf6091b488
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-15 03:14:48 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (84.76%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,47 +1,27 @@
 class Solution {
-    
-public: 
-//     void find(int ind,int target,vector<int> &arr,vector<vector<int>> &ans, vector<int> &ds) {
-//         if(ind == arr.size()){
-//             if(target == 0){
-//                 ans.push_back(ds);
-//             }
-//             return;
-//         }
-        
-//         if(arr[ind]<=target){
-//             ds.push_back(arr[ind]);
-//             find(ind, target-arr[ind],arr,ans,ds);
-//             ds.pop_back();
-//         }
-//         find(ind+1, target,arr,ans,ds);
-//     }
-    
-    void find(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
-        if(ind == arr.size()){
-            if(target == 0){
-                ans.push_back(ds);
-                
+public:
+    void f(int i,int k,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
+        if(i == nums.size()){
+            if(k == 0){
+                ans.push_back(temp);
             }
             return;
         }
-        if(arr[ind]<= target){
-            ds.push_back(arr[ind]);
-            find(ind,target-arr[ind],arr,ans,ds);
-            ds.pop_back();
-            
+        
+        if(k>=nums[i]){
+            temp.push_back(nums[i]);
+            f(i,k-nums[i],nums,temp,ans);
+            temp.pop_back();
         }
         
-        find(ind+1,target,arr,ans,ds);
-        
-        
+        f(i+1,k,nums,temp,ans);
     }
-    
-public:
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        find(0,target,arr,ans,ds);
+    vector<vector<int>> combinationSum(vector<int>& nums, int k) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        int n = nums.size();
+        f(0,k,nums,temp,ans);
+        
         return ans;
     }
 };
