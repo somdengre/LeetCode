efff75010a177e97183b3e589eaf23a851ee9e2b
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-14 20:17:46 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (44.07%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double res = 1;
-        int k = n;
-        while (k) {
-            if (k%2) res *= x;
-            x *= x;            
-            k /= 2;
+         double ans = 1.0;
+        long long nn = n;
+        
+        if(nn<0)nn*=-1;
+        
+        while(nn){
+            if(nn %2 == 1){
+                ans = ans*x;
+                nn= nn-1;
+            }else{
+                x = x*x;
+                nn/=2;
+            }
         }
-        return n > 0 ? res : 1/res;
+        
+        if(n<0)ans = (double)(1.0)/(double)(ans);
+        return ans;
+        
+        
     }
 };
