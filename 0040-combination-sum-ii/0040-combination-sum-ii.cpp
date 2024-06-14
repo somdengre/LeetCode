437f8aa0af5fb302c4f49210dc1d3948618513a1
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-15 03:43:41 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (96.91%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    void f(int ind,int k,vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp){
         
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>> ans;
-        vector<int> ds;
-        findCombination(0,target,candidates,ans,ds);
-        return ans;
+            if(k == 0){
+                ans.push_back(temp);
+                return;
+            }
+            
+        
+        
+        for(int i = ind;i<nums.size();i++){
+            if(i>ind && nums[i] == nums[i-1])continue;
+            if(nums[i] > k)break;
+            temp.push_back(nums[i]);
+            f(i+1,k-nums[i],nums,ans,temp);
+            temp.pop_back();
+        }
     }
-
-    void findCombination(int index, int target, vector<int>& candidates, vector<vector<int>>& ans, vector<int>& ds ){
-
-        if(target==0)
-        {
-            ans.push_back(ds);
-            return;
-        }
-
-        for(int i=index;i<candidates.size();i++)
-        {
-            if(i>index && candidates[i]==candidates[i-1]) continue;
-            if(candidates[i]>target) break;
-            ds.push_back(candidates[i]);
-            findCombination(i+1,target-candidates[i],candidates,ans,ds);
-            ds.pop_back();
-
-
-        }
+    vector<vector<int>> combinationSum2(vector<int>& nums, int k) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        sort(nums.begin(),nums.end());
+        f(0,k,nums,ans,temp);
+        
+        return ans;
     }
 };
