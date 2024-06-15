03803245e31ca1bb2ae2096f380ce214e2ad0bd8
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-16 00:03:35 +0530

    Time: 232 ms (77.18%), Space: 9.7 MB (73.23%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,34 +1,48 @@
-
 class Solution {
-    int dr1[4]={1,-1,0,0};
-    int dr2[4]={0,0,1,-1};
 public:
-    bool f(vector<vector<char>>& board,int& row,int& col,int i,int j,string& word,int ind,int n){
-        if(ind == n-1 && word[ind] == board[i][j]){
+    bool f(int i, int j, vector<vector<char>>& nums, vector<vector<int>>& vis, string& k, int t) {
+        int n = nums.size();
+        int m = nums[0].size();
+        if (t == k.length()) {
             return true;
         }
-        for(int d = 0;d<4;d++){
-            int x = i + dr1[d],y = j + dr2[d];
-            if(x>=0&&x<row&&y>=0&&y<col && board[x][y]!='.' && board[x][y]==word[ind]){
-                char ch = board[x][y];
-                board[x][y] = '.';
-                if(ind == n-1)return true;
-                if(f(board,row,col,x,y,word,ind+1,n) == true)return true;
-                board[x][y] = ch;
-            }
+
+        // Check boundaries and character match
+        if (i < 0 || j < 0 || i >= n || j >= m || vis[i][j] == 1 || nums[i][j] != k[t]) {
+            return false;
         }
-        
+
+        // Mark the current cell as visited
+        vis[i][j] = 1;
+
+        // Explore all four possible directions
+        if (f(i + 1, j, nums, vis, k, t + 1) ||
+            f(i - 1, j, nums, vis, k, t + 1) ||
+            f(i, j + 1, nums, vis, k, t + 1) ||
+            f(i, j - 1, nums, vis, k, t + 1)) {
+            return true;
+        }
+
+        // Backtrack
+        vis[i][j] = 0;
         return false;
     }
-    bool exist(vector<vector<char>>& board, string word) {
-        int n = word.length();
-        int row = board.size();
-        int col = board[0].size();
-        for(int i = 0;i<board.size();i++){
-            for(int j = 0;j<board[i].size();j++){
-                if(f(board,row,col,i,j,word,0,n)==true)return true;
+
+    bool exist(vector<vector<char>>& nums, string k) {
+        int n = nums.size();
+        int m = nums[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (nums[i][j] == k[0]) {
+                    if (f(i, j, nums, vis, k, 0)) {
+                        return true;
+                    }
+                }
             }
         }
+
         return false;
     }
 };
