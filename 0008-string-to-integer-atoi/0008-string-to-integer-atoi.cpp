7842258ecd4b68d0b1dae298f15119887e9ff21e
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-17 20:31:52 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (37.10%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,35 +1,38 @@
-
-
 class Solution {
 public:
-    int f(int i,string &s,long long &ans,int sign){
-        if(i >=s.length())return (int)ans;
+    int f(int i,long long &ans,string&s,int sign){
+        if(i >=s.length()){
+            return (int)ans;
+        }
         
-        if(s[i] < '0' || s[i] > '9')return (int)ans;
+        if(s[i] < '0' || s[i] > '9'){
+            return (int)ans;
+        }
         
         ans = ans*10 + s[i]-'0';
-        if(sign * ans > INT_MAX)return INT_MAX;
-        if(sign*ans < INT_MIN)return INT_MIN; 
-        return f(i+1,s,ans,sign);
+        
+        if(ans*sign > INT_MAX)return INT_MAX;
+        if(ans*sign<INT_MIN)return INT_MIN;
+        
+        return f(i+1,ans,s,sign);
     }
     int myAtoi(string s) {
-        int n = s.length();
         int i = 0;
-        long long ans = 0;
+        int n = s.length();
         int sign = 1;
-        
-        // Skip leading whitespaces
-        while (i < n && s[i] == ' ') {
+        while(i<n && s[i] == ' '){
             i++;
         }
         
-        // Check for sign
         if (i < n && (s[i] == '-' || s[i] == '+')) {
-            sign = (s[i] == '-') ? -1 : 1;
+            if (s[i] == '-') {
+                sign = -1;
+            }
             i++;
         }
         
-        ans = f(i,s,ans,sign);
-        return (int)(sign * ans);
+        long long ans = 0;
+        ans = f(i,ans,s,sign);
+        return (int)(ans*sign);
     }
 };
