f3ae5253d470935ee352cd250ee075ef7c28fada
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-20 03:39:09 +0530

    Time: 4 ms (70.89%), Space: 11.3 MB (70.46%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>mp;
-        for(int i = 0;i<nums2.size();i++){
-            mp[nums2[i]] = i;
-        }
-        
-        int i = 0;
-        while(i<nums1.size()){
-            int p = nums1[i];
-            int z = mp[p];
-            int flag = 0;
-            for(int j = z+1;j<nums2.size();j++){
-                if(nums2[j] > p){
-                    nums1[i] = nums2[j];
-                    flag = 1;
+        map<int,int>mp;
+        int n =nums2.size();
+        int m = nums1.size();
+        for(int i = 0;i<n;i++){
+            int next = -1;
+            for(int j = i+1;j<n;j++){
+                if(nums2[j] > nums2[i]){
+                    next = nums2[j];
                     break;
                 }
+                
             }
-            if(flag == 0){
-                nums1[i] = -1;
-            }
-            i++;
+            mp[nums2[i]] = next;
+        }
+        for(int i = 0;i<m;i++){
+            nums1[i] = mp[nums1[i]];
         }
         
         return nums1;
