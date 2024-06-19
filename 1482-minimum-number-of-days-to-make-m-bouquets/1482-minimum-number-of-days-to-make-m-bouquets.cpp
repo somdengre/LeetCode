ace0407b696fe358b8a5d49c041cc91a604bf977
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-19 20:26:57 +0530

    Time: 113 ms (54.89%), Space: 69 MB (28.78%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool possible(vector<int>&nums,int j,int m,int k){
+    int Possible(int mid,vector<int>&nums,int n,int k,int m){
         int cnt = 0;
-        int n = 0;
-        
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] <= j){
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i] <= mid){
                 cnt++;
             }else{
-                n+=cnt/k;
+                ans+=cnt/k;
                 cnt = 0;
             }
         }
-        n+=(cnt/k);
-        return n >= m;
+        ans+=cnt/k;
+        return ans>=m;
     }
     int minDays(vector<int>& nums, int m, int k) {
         int n = nums.size();
-        if(m>n/k)return -1;
+        if(n/k < m)return -1;
         int mini = INT_MAX;
         int maxi = INT_MIN;
-        int ans = -1;
+        
+        
         for(int i = 0;i<n;i++){
             mini = min(mini,nums[i]);
             maxi = max(maxi,nums[i]);
@@ -28,17 +28,17 @@ public:
         
         int low = mini;
         int high = maxi;
-        
+        int ans = -1;
         while(low<=high){
             int mid = (low+high)/2;
-            
-            if(possible(nums,mid,m,k) == true){
+            if(Possible(mid,nums,n,k,m) == true){
                 ans = mid;
                 high = mid-1;
             }else{
                 low = mid+1;
             }
         }
+        
         return ans;
     }
 };
