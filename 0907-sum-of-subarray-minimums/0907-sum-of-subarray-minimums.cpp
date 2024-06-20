21fb186bb1999c65317cfb2ad2287376c3ffbbac
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-21 03:21:41 +0530

    Time: 64 ms (62.98%), Space: 46.5 MB (25.34%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        int sum=0;
-        for(int i=0; i<n; i++){
-            int curr_index=i;
-            int x=0;
-            int y=0;
-            while(curr_index>=1){
-                if(arr[curr_index-1]>arr[i]){
-                    y++;
-                    curr_index--;
-                }
-                else
-                    break;
+    int mod = 1e9+7;
+    int sumSubarrayMins(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>left(n,0),right(n,0);
+        stack<pair<int,int>>sleft,sright;
+        
+        for(int i =0;i<n;i++){
+            int cnt = 1;
+            while(!sleft.empty() && sleft.top().first > nums[i]){
+                cnt+=sleft.top().second;
+                sleft.pop();
             }
-            curr_index=i;
-            while(curr_index<n-1){
-                if(arr[i]<=arr[curr_index+1]){
-                    x++;
-                    curr_index++;
-                }
-                else
-                    break;
-            }
-            int m = 1000000007;
-            long long r = (((x+1)%m)*((y+1)%m))%(1000000007);
-            long long p = (r*(arr[i]))%(1000000007);
-            sum = (sum+p)%(1000000007);
+            
+            sleft.push({nums[i],cnt});
+            left[i] = cnt;
         }
-        return sum;
+        
+        for(int i = n-1;i>=0;i--){
+            int cnt = 1;
+            while(!sright.empty() && sright.top().first >= nums[i]){
+                cnt+=sright.top().second;
+                sright.pop();
+            }
+            
+            sright.push({nums[i],cnt});
+            right[i] = cnt;
+        }
+         long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            long long product = ((long long)nums[i] * left[i] % mod * right[i] % mod) % mod;
+            ans = (ans + product) % mod;
+        }
+
+        return ans;
+        
     }
 };
