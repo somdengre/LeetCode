c335a1799208a5f4b6c3a211a20c0a41eac8ff2c
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-21 23:38:30 +0530

    Time: 9 ms (61.30%), Space: 20.4 MB (71.39%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& nums) {
+        int n = nums.size();
+        stack<int>st;
+        
+        for(int i = 0;i<n;i++){
+            if(nums[i] > 0 || st.empty() ){
+                st.push(nums[i]);
+            }else{
+                while(!st.empty() && st.top() > 0 && st.top() < abs(nums[i])){
+                    st.pop();
+                }
+                if(!st.empty() && st.top() == abs(nums[i])){
+                    st.pop();
+                }else{
+                    if(st.empty() || st.top() < 0){
+                        st.push(nums[i]);
+                    }
+                }
+            }
+        }
+        
+        vector<int>ans;
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
