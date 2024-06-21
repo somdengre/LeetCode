daaf9993e1a6dd32dbcef5da678cb7d13de0396c
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-22 01:05:01 +0530

    Time: 6 ms (93.98%), Space: 9.9 MB (68.94%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,29 +1,38 @@
 class Solution {
- public:
-  string removeKdigits(string num, int k) {
-    if (num.length() == k)
-      return "0";
-
-    string ans;
-    vector<char> s;
-
-    for (int i = 0; i < num.length(); ++i) {
-      while (k > 0 && !s.empty() && s.back() > num[i]) {
-        s.pop_back();
-        --k;
-      }
-      s.push_back(num[i]);
+public:
+    string removeKdigits(string nums, int k) {
+        int n = nums.size();
+        if(k == n)return "0";
+        
+        stack<char>st;
+        
+        for(int i =0;i<n;i++){
+            while(!st.empty() && st.top() > nums[i] && k>0){
+                st.pop();
+                k--;
+            }
+            
+            if(!st.empty() || nums[i]!= '0'){
+                st.push(nums[i]);
+            } 
+             
+        }
+         while(!st.empty() && k>0){
+            st.pop();
+            k--;
+        }
+        if(st.empty()){
+            return "0";
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
     }
-
-    while (k-- > 0)
-      s.pop_back();
-
-    for (const char c : s) {
-      if (c == '0' && ans.empty())
-        continue;
-      ans += c;
-    }
-
-    return ans.empty() ? "0" : ans;
-  }
 };
