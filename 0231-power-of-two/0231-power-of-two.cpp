7f0eebb19b7a5c40681984d7dce8a3e75b90b55d
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-01 02:53:20 +0530

    Time: 5 ms (20.40%), Space: 7.2 MB (36.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n == 1){
-            return true;
-        }
-        if(n<=0){
-            return false;
-        }
-        long long int p = 1;
-        for(int i = 1;i<32;i++){
-            p=p*2;
-            if(p == n){
-                return true;
-            }
-        }
-        return false;
+        if(n<=0)return false;
+        return (n&(n-1))==0;
     }
 };
