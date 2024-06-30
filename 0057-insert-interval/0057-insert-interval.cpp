badd7cac8821f7c7ce6f347f41253179830b4a55
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-06-30 19:46:49 +0530

    Time: 7 ms (92.07%), Space: 20.4 MB (74.12%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> merged;
+    vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& newin) {
+        vector<vector<int>>ans;
+        int n = nums.size();
         int i = 0;
-
-        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
-            merged.push_back(intervals[i]);
+        while(i<n && nums[i][1] < newin[0] ){
+            ans.push_back(nums[i]);
             i++;
         }
-
-        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
-            newInterval = {min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])};
+        
+        while(i<n && nums[i][0] <= newin[1]){
+            newin[0] = min(newin[0],nums[i][0]);
+            newin[1] = max(newin[1],nums[i][1]);
+            i++;
+            
+        }
+        
+        ans.push_back(newin);
+        
+        while(i<n){
+            ans.push_back(nums[i]);
             i++;
         }
-        merged.push_back(newInterval);
-
-        while (i < intervals.size()) {
-            merged.push_back(intervals[i]);
-            i++;
-        }
-
-        return merged;
+        
+        return ans;
+        
+        
     }
 };
