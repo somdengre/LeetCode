39b866b332f564733dfe2d2b06e38f02f7b4010f
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-09 12:34:23 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (46.70%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,39 +9,18 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-         if(root == NULL)return {};
-        vector<int>ans;
-        TreeNode* cur = root;
+    void f(TreeNode* root,vector<int>&ans){
+        if(root == NULL)return ;
         
-        while(cur){
-            if(cur->left == NULL){
-                ans.push_back(cur->val);
-                cur = cur->right;
-                //if cur->left is null move to right
-            }else{
-                TreeNode* prev = cur->left;
-                //will try to move to the last element of the left subtree
-                while(prev->right && prev->right != cur){
-                    prev = prev->right;
-                }
-                
-                if(prev->right == NULL){
-                    //make the thread
-                    prev->right = cur;
-                    ans.push_back(cur->val); // only change from inorder
-                    cur = cur->left;
-                }else{
-                    //remove the thread
-                    prev->right = NULL;
-                    
-                    cur = cur->right;
-                }
-           }
-        }
+        ans.push_back(root->val);
+        f(root->left,ans);
+        f(root->right,ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int>ans;
+        f(root,ans);
         return ans;
     }
 };
