9203003ff9942ed2b1ba717bbe28366f02d131d8
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-11 02:52:20 +0530

    Time: 9 ms (51.89%), Space: 8.2 MB (80.20%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,19 +3,23 @@ public:
     int characterReplacement(string s, int k) {
         int n = s.length();
         int maxi = 0;
-        for(int i =  0;i<n;i++){
-            vector<int>hash(26,0);
-            int maxfre = 0;
-            for(int j = i;j<n;j++){
-                hash[s[j]-'A']++;
-                maxfre = max(maxfre,hash[s[j]-'A']);
-                int changes = (j-i+1)-maxfre;
-                if(changes <= k){
-                    maxi = max(maxi,j-i+1);
-                }else{
-                    break;
-                }
+        int l = 0,r = 0,maxf = 0;
+        vector<int>hash(26,0);
+        while(r<n){
+            hash[s[r]-'A']++;
+            maxf = max(maxf,hash[s[r]-'A']);
+            
+            while((r-l+1)-maxf > k){
+                hash[s[l]-'A']--;
+                for(int i = 0;i<26;i++)maxf = max(maxf,hash[i]);
+                l++;
             }
+            
+            if((r-l+1)-maxf <= k){
+                maxi = max(maxi,r-l+1);
+            }
+            
+            r++;
         }
         
         return maxi;
