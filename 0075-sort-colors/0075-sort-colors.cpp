6a2bed400a3c5bfd460fe4cd52b2a001957ab724
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-13 00:20:59 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (7.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int low = 0;
-        int mid = 0;
-        int high = n-1;
+        int low = 0,high = n-1,mid = 0;
         
         while(mid<=high){
             if(nums[mid] == 0){
-                swap(nums[low],nums[mid]);
+                swap(nums[mid],nums[low]);
                 low++;
                 mid++;
             }else if(nums[mid] == 1){
@@ -17,6 +15,6 @@ public:
                 swap(nums[mid],nums[high]);
                 high--;
             }
-        }
+        } 
     }
 };
