5ace4eb9e6ca9dc026478ddf31ca83d87db9c0d8
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-14 01:38:23 +0530

    Time: 9 ms (58.65%), Space: 16.8 MB (13.45%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,22 +3,38 @@ public:
     void setZeroes(vector<vector<int>>& nums) {
         int n = nums.size();
         int m = nums[0].size();
-        vector<int>row(n,0),col(m,0);
-        
+        int col0 = 1;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(nums[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+                    nums[i][0] = 0;
+                    if(j!=0){
+                        nums[0][j] = 0;
+                    }else{
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+        for(int i = 1;i<n;i++){
+            for(int j = 1;j<m;j++){
+                if(nums[i][0] == 0 || nums[0][j] == 0){
+                    nums[i][j] = 0;
                 }
             }
         }
         
-        for(int i = 0;i<n;i++){
+        if(nums[0][0] == 0){
             for(int j = 0;j<m;j++){
-                if(row[i] == 1 || col[j] == 1)nums[i][j] = 0;
+                nums[0][j] = 0;
             }
         }
-
+        
+        if(col0 == 0){
+            for(int i = 0;i<n;i++){
+                nums[i][0] = 0;
+            }
+        }
+        
     }
 };
