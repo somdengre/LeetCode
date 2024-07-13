4f1b934f47d1e63e522dc23bf80412481a3fe8e0
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-14 00:45:11 +0530

    Time: 11 ms (46.77%), Space: 17 MB (6.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& nums) {
-        unordered_set<int>row,col;
         int n = nums.size();
         int m = nums[0].size();
-        int col0 = 1;
+        vector<int>row(n,0),col(m,0);
+        
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(nums[i][j] == 0){
-                    nums[i][0] = 0;
-                    if(j!= 0){
-                        nums[0][j] = 0;
-                    }else{
-                        col0 = 0;
-                    }
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
         
-        for(int i = 1;i<n;i++){
-            for(int j = 1;j<m;j++){
-                if(nums[i][0] == 0 || nums[0][j] == 0){
-                    nums[i][j] = 0;
-                }
-            }
-        }
-        
-        if(nums[0][0] == 0){
+        for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                nums[0][j] = 0;
+                if(row[i] == 1 || col[j] == 1)nums[i][j] = 0;
             }
         }
-        
-        if(col0 == 0){
-            for(int i = 0;i<n;i++){
-                nums[i][0] = 0;
-            }
-        }
-        
+
     }
 };
