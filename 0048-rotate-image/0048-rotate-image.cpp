3a16fd5539a7702e486d5e65d7e2eb7469092443
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-14 01:48:02 +0530

    Time: 4 ms (38.24%), Space: 9 MB (24.22%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,13 +4,12 @@ public:
         int n = nums.size();
         
         for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                swap(nums[i][j],nums[j][i]);
+            for(int j = 0;j<n;j++){
+                if(i!=j && i>j){
+                    swap(nums[i][j],nums[j][i]);
+                }
             }
         }
-        
-        for(int i = 0;i<n;i++){
-            reverse(nums[i].begin(),nums[i].end());
-        }
+        for(int i = 0;i<n;i++)reverse(nums[i].begin(),nums[i].end());
     }
 };
