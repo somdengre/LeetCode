ae7d0189d4bfc50bfdc51150aac5a6145cb7a160
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-14 12:45:49 +0530

    Time: 7 ms (81.50%), Space: 18.6 MB (30.90%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,37 +2,33 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int el1 = INT_MIN;
-        int el2 = INT_MIN;
+        int cnt1 = 0,cnt2 = 0;
+        int el1 = INT_MIN,el2 = INT_MIN;
+        
         for(int i = 0;i<n;i++){
-            if(cnt1 == 0 && nums[i] != el2){
-                cnt1 = 1;
-                el1 = nums[i];
-            }else if(cnt2 == 0 && nums[i] != el1){
-                cnt2 = 1;
-                el2 = nums[i];
+            if(cnt1 == 0 && el2 != nums[i]){
+                cnt1 = 1;el1 = nums[i];
+            }else if(cnt2 == 0 && nums[i]!=el1){
+                cnt2 = 1;el2 = nums[i];
             }else if(nums[i] == el1){
                 cnt1++;
             }else if(nums[i] == el2){
                 cnt2++;
             }else{
-                cnt1--;
-                cnt2--;
+                cnt1--;cnt2--;
             }
         }
         
-        vector<int>ans;
         cnt1 = 0;
         cnt2 = 0;
+        
         for(int i = 0;i<n;i++){
             if(nums[i] == el1)cnt1++;
             if(nums[i] == el2)cnt2++;
         }
-        int mini = int(n/3) + 1; 
-        if(cnt1>=mini)ans.push_back(el1);
-        if(cnt2>=mini)ans.push_back(el2);
+        vector<int>ans;
+        if(cnt1 > n/3)ans.push_back(el1);
+        if(cnt2>n/3)ans.push_back(el2);
         
         return ans;
     }
