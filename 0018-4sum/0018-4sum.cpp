8e23e009b4086fa123f96b1e8557cf4fb4b27681
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-14 19:08:41 +0530

    Time: 26 ms (64.90%), Space: 16.2 MB (29.76%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> fourSum(vector<int>& nums, int t) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-        
+        sort(nums.begin(),nums.end());
         for(int i = 0;i<n;i++){
             if(i>0 && nums[i] == nums[i-1])continue;
+            
             for(int j = i+1;j<n;j++){
-                if(j!=i+1 && nums[j-1] == nums[j])continue;
+                if(j!=i+1 && nums[j] == nums[j-1])continue;
                 int k = j+1;
                 int l = n-1;
                 
@@ -17,17 +17,17 @@ public:
                     sum+=nums[j];
                     sum+=nums[k];
                     sum+=nums[l];
-                    if(sum == target){
+                    if(sum>t){
+                        l--;
+                    }else if(sum<t){
+                        k++;
+                    }else{
                         vector<int>temp = {nums[i],nums[j],nums[k],nums[l]};
                         ans.push_back(temp);
                         k++;
                         l--;
-                        while(k<l && nums[k-1] == nums[k])k++;
-                        while(k<l && nums[l+1] == nums[l])l--;
-                    }else if(sum< target){
-                        k++;
-                    }else{
-                        l--;
+                        while(k<l && nums[k]==nums[k-1])k++;
+                        while(k<l && nums[l] == nums[l+1])l--;
                     }
                 }
             }
