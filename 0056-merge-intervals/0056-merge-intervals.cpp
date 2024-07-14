dd323e030a6db06c783f99ed5143bffc8f1ea871
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-14 20:33:24 +0530

    Time: 19 ms (85.52%), Space: 23 MB (27.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,24 +2,16 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
         for(int i = 0;i<n;i++){
-            int st = nums[i][0];
-            int en = nums[i][1];
-            
-            if(!ans.empty() && en<=ans.back()[1])continue;
-            
-            for(int j = i+1;j<n;j++){
-                if(nums[j][0] <=en){
-                    en = max(en,nums[j][1]);
-                }else{
-                    break;
-                }
+            if(!ans.empty() && ans.back()[1] >= nums[i][0]){
+                ans.back()[1] = max(ans.back()[1],nums[i][1]);
+            }else{
+                ans.push_back(nums[i]);
             }
-            
-            ans.push_back({st,en});
         }
+        
         return ans;
     }
 };
