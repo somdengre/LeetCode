e5f3cb17ab4ae32a8d3e98693f2bcba31560ca7e
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-19 02:06:46 +0530

    Time: 123 ms (62.13%), Space: 49.3 MB (9.41%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    int f(vector<vector<int>>&nums,int n,int m,int mid){
+    int f(vector<vector<int>>&nums,int m,int n,int col){
+        int row = -1;
         int maxi = INT_MIN;
-        int index = -2;
         for(int i = 0;i<n;i++){
-            if(nums[i][mid] > maxi){
-                maxi = nums[i][mid];
-                index = i;
-            }   
+            if(maxi<nums[i][col]){
+                maxi = max(maxi,nums[i][col]);
+                row = i;
+            }
         }
-        return index;
+        
+        return row;
     }
     vector<int> findPeakGrid(vector<vector<int>>& nums) {
-        int n =nums.size();
+        int n = nums.size();
         int m = nums[0].size();
+        
         
         int low = 0;
         int high = m-1;
+        
         while(low<=high){
             int mid = (low+high)/2;
-            
-            int row = f(nums,n,m,mid);
-            
-            int left = mid-1 >=0?nums[row][mid-1]:-1;
-            int right = mid+1< m ? nums[row][mid+1]:-1;
-            
-            if(nums[row][mid]>left &&  nums[row][mid]>right){
+            int row = f(nums,m,n,mid);
+            int left = mid-1 >=0 ? nums[row][mid-1]: -1;
+            int right = mid+1 < m ? nums[row][mid+1]:-1;
+            if(nums[row][mid] > left && nums[row][mid]>right){
                 return {row,mid};
-            }else if(nums[row][mid] < left){
+            }else if(nums[row][mid]<left){
                 high = mid-1;
             }else{
                 low = mid+1;
             }
+            
         }
         
         return {-1,-1};
