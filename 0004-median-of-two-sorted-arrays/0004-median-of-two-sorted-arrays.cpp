7c93acfedf94a071de2e72d3f4bfbd80becc8ced
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-18 20:07:40 +0530

    Time: 35 ms (15.43%), Space: 95.6 MB (21.39%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    int INF = -100000000;
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size()){
-            return findMedianSortedArrays(nums2, nums1);
-        }
-
-        int l1 = nums1.size(), l2 = nums2.size();
-        int low = 0, high = l1, partition1, partition2;
-        int maxL1, maxL2, minR1, minR2;
-        float ret = 0.0;
-
-        while(low <= high){
-            partition1 = (low+high)/2;
-            partition2 = ((l1+l2+1)/2) - partition1;
-
-            maxL1 = (partition1 == 0) ? INF : nums1[partition1 - 1];
-            minR1 = (partition1 == l1) ? -INF : nums1[partition1];
-            maxL2 = (partition2 == 0) ? INF : nums2[partition2 - 1]; 
-            minR2 = (partition2 == l2) ? -INF : nums2[partition2];
-
-            //cout<<partition1<<" "<<partition2<<" "<<maxL1<<" "<<minR1<<" "<<maxL2<<" "<<minR2<<endl;
-            if(maxL1 <= minR2 && maxL2 <= minR1){
-                if((l1+l2)%2 == 0) ret = (max(maxL1, maxL2) + min(minR1, minR2))*1.0/ 2.0;
-                else ret = max(maxL1, maxL2);
-
-                return ret;
-            } else if(maxL1 > minR2) 
-                high = partition1 - 1;
-            else 
-                low = partition1 + 1;        
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<int>temp;
+        int i = 0,j = 0;
+        while(i<n && j<m){
+            if(nums1[i] < nums2[j]){
+                temp.push_back(nums1[i++]);
+            }else{
+                temp.push_back(nums2[j++]);
+            }
         }
         
-        return ret;
+        while(i<n){
+            temp.push_back(nums1[i]);
+            i++;
+        }
+        while(j<m){
+            temp.push_back(nums2[j]);
+            j++;
+        }
+        
+        int size = n+m;
+        if(size%2 == 1){
+            return temp[size/2];
+        }else{
+            return (double)(temp[size/2]+temp[size/2 -1])/2.0;
+        }
     }
 };
