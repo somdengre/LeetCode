0496c1e29a1dd2d2e6da3f8ff7a4209172375efa
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-19 19:39:05 +0530

    Time: 13 ms (26.16%), Space: 8.8 MB (46.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length())return false;
-        map<char,int>mp;
-        int n = s.length();
-        for(int i = 0;i<n;i++){
-            mp[s[i]]++;
-        }
-        
-        for(int i = 0;i<n;i++){
-            if(mp.find(t[i]) != mp.end()){
-                mp[t[i]]--;
-                if(mp[t[i]] == 0){
-                    mp.erase(t[i]);
-                }
-            }else{
-                return false;
-            }
-        }
-        
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return t==s;
     }
 };
