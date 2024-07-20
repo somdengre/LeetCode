1f0f3f77deb3645207347880476357956f42713b
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-20 16:41:48 +0530

    Time: 8 ms (12.40%), Space: 10.8 MB (5.83%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
-    int f(int i,long long &ans,string&s,int sign){
-        if(i >=s.length()){
-            return (int)ans;
-        }
-        
+    int f(string s,int i,long long &ans,int sign){
+        if(i>=s.length())return (int)ans;
         if(s[i] < '0' || s[i] > '9'){
             return (int)ans;
         }
+        ans = ans*10+(s[i]-'0');
         
-        ans = ans*10 + s[i]-'0';
+        if(ans*sign >= INT_MAX)return INT_MAX;
+        if( ans*sign <= INT_MIN)return INT_MIN;
         
-        if(ans*sign > INT_MAX)return INT_MAX;
-        if(ans*sign<INT_MIN)return INT_MIN;
-        
-        return f(i+1,ans,s,sign);
+        return f(s,i+1,ans,sign);
     }
     int myAtoi(string s) {
         int i = 0;
         int n = s.length();
-        int sign = 1;
         while(i<n && s[i] == ' '){
             i++;
         }
-        
-        if (i < n && (s[i] == '-' || s[i] == '+')) {
-            if (s[i] == '-') {
+        int sign = 1;
+        if(i<n && (s[i] == '+' || s[i] == '-')){
+            if(s[i] == '+'){
+                sign = 1;
+            }else {
                 sign = -1;
             }
             i++;
         }
         
-        long long ans = 0;
-        ans = f(i,ans,s,sign);
+        long long  ans = 0;
+        ans = f(s,i,ans,sign);
         return (int)(ans*sign);
     }
 };
