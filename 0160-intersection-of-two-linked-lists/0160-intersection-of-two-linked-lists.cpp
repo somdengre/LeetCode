f4819375afd1dea3832979ff5a424ff7fc72b29e
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-21 16:04:12 +0530

    Time: 35 ms (81.39%), Space: 17.4 MB (22.36%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,43 +8,41 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA == NULL || headB == NULL)return NULL;
-        int n1 = 0;
-        int n2 = 0;
-        ListNode* temp1 = headA;
+    int len(ListNode*head){
+        ListNode*temp = head;
+        int cnt = 0;
         
-        while(temp1){
-            n1++;
-            temp1 = temp1->next;
+        while(temp){
+            cnt++;
+            temp = temp->next;
+        }
+        return cnt;
+    }
+    ListNode* f(ListNode* head1,ListNode*head2,int d){
+        ListNode* temp1 = head1;
+        ListNode*temp2 = head2;
+        
+        while(d){
+            temp2 = temp2->next;
+            d--;
         }
         
-        temp1 = headB;
-        while(temp1){
-            n2++;
-            temp1=temp1->next;
-        }
-        temp1 = headA;
-        ListNode* temp2 = headB;
-        if(n1>n2){
-            for(int i = 0;i<n1-n2;i++){
-                temp1 = temp1->next;
-            }
-        }else if(n2>n1){
-             for(int i = 0;i<n2-n1;i++){
-                temp2 = temp2->next;
-            }
-        }
-        
-        while(temp1 && temp2){
-            if(temp1 == temp2){
-                return temp1;
-            }
-            
+        while(temp1 != temp2){
             temp1 = temp1->next;
             temp2 = temp2->next;
         }
         
-        return NULL;
+        return temp1;
+    }
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
+        int n1 = len(head1);
+        int n2 = len(head2);
+        
+        if(n1>n2){
+            return f(head2,head1,n1-n2);
+        }else{
+            return f(head1,head2,n2-n1);
+        }
+        
     }
 };
