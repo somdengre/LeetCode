83243c57e5ed7ff7c901a564b9e20e97bf05c1ef
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-22 00:53:43 +0530

    Time: 3 ms (58.09%), Space: 9 MB (16.04%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    
     int myAtoi(string s) {
         int i = 0;
         int n = s.length();
+        
         while(i<n && s[i] == ' '){
             i++;
         }
         int sign = 1;
+        
         if(i<n && (s[i] == '+' || s[i] == '-')){
-            if(s[i] == '+'){
-                sign = 1;
-            }else {
+            if(s[i] == '-'){
                 sign = -1;
             }
             i++;
         }
         
-        long long  ans = 0;
+        long long ans = 0;
         
-        for(int j = i;j<n;j++){
-            if(s[j]<'0' || s[j]>'9'){
-                return (int)ans*sign;
+        while(i<n){
+            if(s[i] < '0' || s[i] >'9'){
+                break;
             }
-            ans = ans*10+(s[j]-'0');
-            if(ans*sign > INT_MAX)return INT_MAX;
-            if(ans*sign < INT_MIN)return INT_MIN;
             
+            ans = ans*10 + (s[i] - '0');
             
+            if (sign == 1 && ans > INT_MAX) return INT_MAX;
+            if (sign == -1 && -ans < INT_MIN) return INT_MIN;
+            i++;
         }
         
         return (int)(ans*sign);
