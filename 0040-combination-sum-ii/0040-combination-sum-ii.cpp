4a2652223f2fc995cb588d93c430c0780d814c52
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-23 01:46:17 +0530

    Time: 3 ms (78.28%), Space: 12.8 MB (42.65%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    void f(int ind,int k,vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp){
-        
-            if(k == 0){
-                ans.push_back(temp);
-                return;
-            }
-            
-        
+    void f(int ind ,vector<int>&nums,vector<vector<int>>&ans,int k,vector<int>&temp){
+       if(k == 0){
+           ans.push_back(temp);
+           return;
+       }
         
         for(int i = ind;i<nums.size();i++){
-            if(i>ind && nums[i] == nums[i-1])continue;
-            if(nums[i] > k)break;
+            if(i>ind && nums[i-1]== nums[i])continue;
+            if(nums[i]>k)break;
             temp.push_back(nums[i]);
-            f(i+1,k-nums[i],nums,ans,temp);
+            f(i+1,nums,ans,k-nums[i],temp);
             temp.pop_back();
         }
     }
-    vector<vector<int>> combinationSum2(vector<int>& nums, int k) {
-        vector<vector<int>>ans;
+    vector<vector<int>>combinationSum2(vector<int>& nums, int k) {
         vector<int>temp;
         sort(nums.begin(),nums.end());
-        f(0,k,nums,ans,temp);
-        
+        int n = nums.size();
+        vector<vector<int>>ans;                                                          
+        f(0,nums,ans,k,temp);
         return ans;
     }
 };
