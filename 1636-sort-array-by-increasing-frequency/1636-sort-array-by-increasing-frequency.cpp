efdb8fed15ac5862ae4d9e6e614d506e4b48a425
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-23 21:04:47 +0530

    Time: 0 ms (100.00%), Space: 14.5 MB (83.18%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        map<int,int> mp;
-        for(auto i:nums) mp[i]++;
-        vector<pair<int,int>> temp;
-        for(auto i:mp) temp.push_back({i.second,i.first});
-        sort(temp.begin(),temp.end(),[](pair<int,int> p1,pair<int,int> p2){
-            if(p1.first<p2.first) return true;
-            else if(p1.first==p2.first) return p1.second>p2.second;
-            return false;
+        int freq[201] = {};
+        for (int num : nums)
+            freq[num + 100]++;
+        sort(nums.begin(), nums.end(), [&freq](int a, int b){
+            return freq[a + 100] == freq[b + 100] ? a > b : freq[a + 100] < freq[b + 100];
         });
-        vector<int> ans;
-        for(auto i:temp){
-            while(i.first>0){
-                ans.push_back(i.second);
-                i.first--;
-            }
-        }
-        return ans;
+        return nums;
     }
 };
