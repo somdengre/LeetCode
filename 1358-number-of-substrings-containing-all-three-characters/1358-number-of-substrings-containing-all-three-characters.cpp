4a75c57539a6cbdf10ea9695a69a2250fd23d6b2
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-07-26 01:26:09 +0530

    Time: 19 ms (51.99%), Space: 10 MB (45.03%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -3,15 +3,14 @@ public:
     int numberOfSubstrings(string s) {
         int n = s.length();
         vector<int>temp(3,-1);
-        int ans = 0;
+        int cnt = 0;
         for(int i = 0;i<n;i++){
             temp[s[i]-'a'] = i;
-            if(temp[0]!=-1 && temp[1]!=-1 && temp[2] != -1){
-                ans+=1+min(temp[0],min(temp[1],temp[2]));
+            if(temp[0]!= -1 && temp[1]!=-1 && temp[2]!=-1){
+                cnt+= 1+min(temp[0],min(temp[1],temp[2]));
             }
-            
         }
         
-        return ans;
+        return cnt;
     }
 };
