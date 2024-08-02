9e22e9367d95c4a3f53fac2808b80b1990b27923
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-03 04:06:32 +0530

    Time: 58 ms (99.69%), Space: 43.8 MB (69.88%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int,int>> adj[n + 1];
-        for(auto it : times){
-            adj[it[0]].push_back({it[1], it[2]});
+    int networkDelayTime(vector<vector<int>>& nums, int n, int k) {
+        vector<pair<int,int>>adj[n+1];
+        
+        for(int i = 0;i<nums.size();i++){
+            adj[nums[i][0]].push_back({nums[i][1],nums[i][2]});
         }
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({0 , k});
-        vector<int>dist(n+1 , INT_MAX);
-        dist[k] = 0; 
-
+        pq.push({0,k});
+        vector<int>dist(n+1,1e9);
+        dist[k] = 0;
         while(!pq.empty()){
-            int dis = pq.top().first; 
-            int node = pq.top().second;
+            auto it = pq.top();
             pq.pop();
-
-           
-            for(auto it : adj[node]){
-                int adjNode = it.first; 
-                int wt = it.second; 
-
-                if(dis + wt < dist[adjNode]){
-                    dist[adjNode] = dis + wt;
-                    pq.push({dist[adjNode] , adjNode });
+            int node = it.second;
+            int time = it.first;
+            
+            for(auto it: adj[node]){
+                int adjNode = it.first;
+                int edw = it.second;
+                
+                if(time+edw < dist[adjNode]){
+                    dist[adjNode] = time+edw;
+                    pq.push({dist[adjNode],adjNode});
                 }
-            }  
+            }
         }
-        int maxi = *max_element(dist.begin()+1 , dist.end() );
-
-        return maxi == INT_MAX ? -1 : maxi;
+        
+        int ans = INT_MIN;
+        for(int i = 1;i<=n;i++){
+            ans = max(ans,dist[i]);
+        }
+        
+        return ans == 1e9? -1: ans;
+        
     }
 };
