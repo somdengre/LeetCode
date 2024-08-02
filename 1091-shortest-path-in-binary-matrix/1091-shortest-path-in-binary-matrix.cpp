cbc27a70f3af87b8d5aeda030d195467ecc1c0cc
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-03 02:42:07 +0530

    Time: 48 ms (54.67%), Space: 25.2 MB (33.24%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,47 +1,45 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>> &grid)
-{
-    int n = grid.size();
-    int m = grid[0].size();
-
-    if (grid[0][0] == 1 or grid[n - 1][m - 1] == 1)
-    {
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+         if (grid[0][0] == 1 || grid[n-1][n-1] == 1) {
+            return -1; 
+        }
+         if (n == 1 && m == 1 && grid[0][0] == 0) {
+            return 1;
+        }
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+        queue<pair<int,pair<int,int>>>q;
+        q.push({1,{0,0}}); //start with a distance of 1
+        dist[0][0] = 0;
+        
+        
+        
+         int dr[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+        int dc[] = {0, 1, 1, 1, 0, -1, -1, -1};
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int dis = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+            
+            for(int i = 0;i<8;i++){
+                int nrow = row+dr[i];
+                int ncol = col+dc[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 0 && dis + 1 < dist[nrow][ncol]){
+                    if(nrow == n-1 && ncol == n-1){
+                        return dis+1;
+                    }
+                    dist[nrow][ncol] = 1 + dis;
+                    q.push({dis+1,{nrow,ncol}});
+                }
+            }
+            
+           
+        }
         return -1;
     }
-
-    vector<vector<int>> visited(n, vector<int>(m, 0));
-    int rr[] = {-1, -1, 0, 1, 1, 1, 0, -1};
-    int cc[] = {0, 1, 1, 1, 0, -1, -1, -1};
-    queue<pair<pair<int, int>, int>> pq;
-    pq.push({{0, 0}, 1});
-    visited[0][0] = 1;
-
-    while (!pq.empty())
-    {
-        int dis = pq.front().second;
-        int row = pq.front().first.first;
-        int col = pq.front().first.second;
-        pq.pop();
-
-        if (row == n - 1 and col == m - 1)
-        {
-            return dis;
-        }
-
-        for (int i = 0; i < 8; i++)
-        {
-            int r = row + rr[i];
-            int c = col + cc[i];
-
-            if (r >= 0 and r < n and c < m and c >= 0 and grid[r][c] == 0 and !visited[r][c])
-            {
-                pq.push({{r, c}, dis + 1});
-                visited[r][c] = 1;
-            }
-        }
-    }
-
-    return -1;
-}
 };
