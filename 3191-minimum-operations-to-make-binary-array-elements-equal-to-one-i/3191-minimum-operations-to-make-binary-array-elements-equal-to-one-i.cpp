557a3d6b9bf4d20b1aff00ec13f800b45f6b17b8
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-03 23:49:42 +0530

    Time: 128 ms (9.30%), Space: 139.1 MB (8.51%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        
         int n = nums.size();
         int ans = 0;
-        for(int i = 0;i<n;i++){
-            if(nums[i] == 1){
-                continue;
-            }else{
-                int j = i;
-                while(j<n && j<=i+2 && i+1 <n && i+2 <n){
-                    nums[j] = !nums[j];
-                    j++;
-                }
+        for(int i = 0;i<n-2;i++){
+            if(nums[i] == 0){
                 ans++;
+                for(int j = i;j<i+3;j++){
+                    
+                    nums[j] = !nums[j];
+                }
             }
         }
         
@@ -22,6 +18,7 @@ public:
                 return -1;
             }
         }
+        
         return ans;
     }
 };
