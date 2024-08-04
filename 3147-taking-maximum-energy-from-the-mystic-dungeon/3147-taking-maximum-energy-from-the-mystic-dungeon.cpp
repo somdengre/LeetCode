0bf137f186411c55d2ea60b1c539e6ea029b530f
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-05 02:56:54 +0530

    Time: 145 ms (57.61%), Space: 156.3 MB (10.87%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
+    int f(int i,vector<int>&nums,vector<int>&dp,int k){
+        if(i>=nums.size()){
+            return 0;
+        }
+        if(dp[i] != -1)return dp[i];
+        
+        return dp[i] = nums[i] + f(i+k,nums,dp,k);
+    }
     int maximumEnergy(vector<int>& nums, int k) {
-        int maxi = INT_MIN;
         int n = nums.size();
-        vector<vector<int>>ans(n+1);
-        int ind = 0;
+        vector<int>dp(n,-1);
+        
+        int maxi = INT_MIN;
         for(int i = 0;i<n;i++){
-            ans[ind].push_back(nums[i]);
-            ind++;
-            if(ind == k)ind = 0;
+            maxi =max(maxi,f(i,nums,dp,k));
         }
-        for(auto it: ans){
-            if(it.size() > 0){
-                int sum = 0;
-                for(int j = it.size()-1;j>=0;j--){
-                    sum+=it[j];
-                    maxi = max(maxi,sum);
-                }
-                
-            }
-        }
-
+        
         return maxi;
     }
 };
