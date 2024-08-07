25ddda5faf7242d07f67074491eb0045ef015cd7
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-08 01:52:32 +0530

    Time: 4 ms (32.49%), Space: 9.9 MB (8.11%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int f(int i,vector<int>nums,vector<int>&dp){
-        if(i == 0)return nums[0];
-        if(i == 1)return max(nums[0],nums[1]);
-        if(dp[i]!= -1)return dp[i];
+    int f(vector<int>nums){
+        int n = nums.size();
+        vector<int>dp(n,0);
+        dp[0] = nums[0];
         
-        int p = nums[i] + f(i-2,nums,dp);
-        int np = f(i-1,nums,dp);
+        for(int i = 1;i<n;i++){
+            int pick = nums[i];
+            if(i>1)pick+=dp[i-2];
+            int npick = dp[i-1];
+            
+            dp[i] = max(pick,npick);
+        }
         
-        return dp[i] =  max(p,np);
+        return dp[n-1];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>nums1,nums2;
-        vector<int>dp1(n,-1);
-        vector<int>dp2(n,-1);
         if(n == 1)return nums[0];
+        vector<int>temp1,temp2;
         for(int i = 0;i<n;i++){
             if(i != 0){
-                nums1.push_back(nums[i]);   
+                temp1.push_back(nums[i]);
             }
-            if(i!=n-1){
-                nums2.push_back(nums[i]);
-            }
+            if(i != n-1)temp2.push_back(nums[i]);
         }
         
-        return max(f(n-2,nums1,dp1),f(n-2,nums2,dp2));
-        
-        
+        return max(f(temp1),f(temp2));
     }
 };
