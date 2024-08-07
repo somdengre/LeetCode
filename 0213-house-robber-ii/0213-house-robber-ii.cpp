03211ae0176f37d772e316fdd7b3a37b4965ad2a
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-08 01:53:56 +0530

    Time: 4 ms (32.49%), Space: 9.9 MB (8.11%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -3,17 +3,19 @@ public:
     int f(vector<int>nums){
         int n = nums.size();
         vector<int>dp(n,0);
-        dp[0] = nums[0];
-        
+        int prev = nums[0];
+        int prev2 = 0;
         for(int i = 1;i<n;i++){
             int pick = nums[i];
-            if(i>1)pick+=dp[i-2];
-            int npick = dp[i-1];
+            if(i>1)pick+=prev2;
+            int npick = prev;
             
-            dp[i] = max(pick,npick);
+            int cur = max(pick,npick);
+            prev2 = prev;
+            prev = cur;
         }
         
-        return dp[n-1];
+        return prev;
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
