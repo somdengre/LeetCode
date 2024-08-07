bba1078ad47a0f1cccf23559463cf74f66b033fa
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-08 02:30:02 +0530

    Time: 0 ms (100.00%), Space: 13 MB (9.99%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    int f(int i,int j,vector<vector<int>>&nums,vector<vector<int>>&dp){
-        if(i == 0 && j == 0){
-            return nums[0][0];
-        }
+//     int f(int i,int j,vector<vector<int>>&nums,vector<vector<int>>&dp){
+//         if(i == 0 && j == 0){
+//             return nums[0][0];
+//         }
         
-        if(dp[i][j] != -1)return dp[i][j];
-        int left = 1e9;
-        if(j>0)left = nums[i][j]+ f(i,j-1,nums,dp);
-        int up = 1e9;
-        if(i>0)up = nums[i][j] + f(i-1,j,nums,dp);
+//         if(dp[i][j] != -1)return dp[i][j];
+//         int left = 1e9;
+//         if(j>0)left = nums[i][j]+ f(i,j-1,nums,dp);
+//         int up = 1e9;
+//         if(i>0)up = nums[i][j] + f(i-1,j,nums,dp);
         
-        return dp[i][j] = min(left,up);
-    }
+//         return dp[i][j] = min(left,up);
+//     }
     int minPathSum(vector<vector<int>>& nums) {
         int n = nums.size();
         int m = nums[0].size();
         
         vector<vector<int>>dp(n,vector<int>(m,-1));
+        dp[0][0] = nums[0][0];
         
-        return f(n-1,m-1,nums,dp);
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(i == 0 && j == 0)continue;
+                int left = 1e9;
+                if(j>0)left = nums[i][j]+ dp[i][j-1];
+                int up = 1e9;
+                if(i>0)up = nums[i][j] + dp[i-1][j];
+        
+                dp[i][j] = min(left,up);
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
