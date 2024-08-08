6f0c73c9713a6a16306d8ae59d296ee906bb0a8d
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-09 02:31:09 +0530

    Time: 47 ms (33.89%), Space: 47.1 MB (38.80%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
-    int f(int i,vector<int>&nums,vector<vector<int>>&dp,int k){
+//     int f(int i,vector<int>&nums,vector<vector<int>>&dp,int k){
         
-        if(i == 0){
-            if(k%nums[i] == 0){
-                return k/nums[i];
-            }else{
-                return 1e8;
-            }
-        }
+//         if(i == 0){
+//             if(k%nums[i] == 0){
+//                 return k/nums[i];
+//             }else{
+//                 return 1e8;
+//             }
+//         }
         
-        if(dp[i][k] != -2)return dp[i][k];
-        int pick = INT_MAX;
-        if(nums[i]<=k)pick = 1+f(i,nums,dp,k-nums[i]);
-        int npick = f(i-1,nums,dp,k);
-        return dp[i][k] = min(pick,npick);
-    }
+//         if(dp[i][k] != -2)return dp[i][k];
+//         int pick = INT_MAX;
+//         if(nums[i]<=k)pick = 1+f(i,nums,dp,k-nums[i]);
+//         int npick = f(i-1,nums,dp,k);
+//         return dp[i][k] = min(pick,npick);
+//     }
     int coinChange(vector<int>& nums, int k) {
         
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(k+1,-2));
-        int ans = f(n-1,nums,dp,k);
+        vector<vector<int>>dp(n,vector<int>(k+1,0));
+        for(int i = 0 ;i<=k;i++){
+            if(i%nums[0] == 0){
+                dp[0][i] = i/nums[0];
+            }else{
+                dp[0][i] = 1e8;
+            }
+        }
+        
+        for(int i = 1;i<n;i++){
+            for(int j = 0;j<=k;j++){
+                int pick = INT_MAX;
+                if(nums[i]<=j)pick = 1+dp[i][j-nums[i]];
+                int npick = dp[i-1][j];
+                dp[i][j] = min(pick,npick);
+            }
+        }
+        int ans = dp[n-1][k];
         if(ans>=1e8){
             return -1;
         }
