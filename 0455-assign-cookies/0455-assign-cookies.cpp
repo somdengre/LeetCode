00ac3ae098271b7f7b20413bf37d3c3f054d3867
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-09 01:52:30 +0530

    Time: 15 ms (93.65%), Space: 20.9 MB (39.73%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,14 +3,13 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        
-        int i = 0,j = 0;
-        int cnt = 0;
-        while(i<g.size() && j<s.size()){
+        int i = 0,j = 0,cnt = 0;
+        int n = g.size(),m = s.size();
+        while(i<n && j<m){
             if(s[j]>=g[i]){
-                cnt++;
                 i++;
                 j++;
+                cnt++;
             }else{
                 j++;
             }
