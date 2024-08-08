822448f126596c4dfd59aa78ddfc9393dd4ed696
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-09 03:50:26 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (62.23%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -7,23 +7,25 @@ public:
         int p = s-k;
         if(p<0 || p&1)return 0;
         p = p/2;
-        vector<vector<int>>dp(n,vector<int>(p+1,0));
+        vector<int>prev(p+1,0);
         
         for(int i = 0;i<=p;i++){
-            if(i == 0 && nums[0] == i)dp[0][i] = 2;
-            else if(i == 0 || nums[0] == i)dp[0][i] = 1;
-            else dp[0][i] = 0;
+            if(i == 0 && nums[0] == i)prev[i] = 2;
+            else if(i == 0 || nums[0] == i)prev[i] = 1;
+            else prev[i] = 0;
         }
         
         for(int i = 1;i<n;i++){
+            vector<int>temp(p+1,0);
             for(int k = 0;k<=p;k++){
                 int pick = 0;
-                if(k >= nums[i])pick = dp[i-1][k-nums[i]];
-                int npick = dp[i-1][k];
-                dp[i][k] = pick+npick;
+                if(k >= nums[i])pick = prev[k-nums[i]];
+                int npick = prev[k];
+                temp[k] = pick+npick;
             }
+            prev = temp;
         }
         
-        return dp[n-1][p];
+        return prev[p];
     }
 };
