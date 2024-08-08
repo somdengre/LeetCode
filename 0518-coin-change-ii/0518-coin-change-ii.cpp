9eff1264908cf74a9a4dbf33245e1e56f765cabb
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-09 04:05:11 +0530

    Time: 5 ms (81.97%), Space: 20.2 MB (24.64%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,45 +1,18 @@
 class Solution {
 public:
-//     int f(int i,int k,vector<int>&nums,vector<vector<int>>&dp){
-//         if(k == 0)return 1;
-//         if(i == 0){
-//             if(k%nums[0] == 0){
-//                 return 1;
-//             }else{
-//                 return 0;
-//             }
-//         }
+    int f(int i,int k,vector<int>&nums,vector<vector<int>>&dp){
+        if(k == 0)return 1;
+        if(i == 0)return (k%nums[i] == 0);
         
-//         if(dp[i][k] != -1)return dp[i][k];
-//         int np = f(i-1,k,nums,dp);
-//         int p = 0;
-//         if(k>=nums[i])p = f(i,k-nums[i],nums,dp);
-        
-//         return dp[i][k] = p+np;
-//     }
+        if(dp[i][k] != -1)return dp[i][k];
+        int pick = 0;
+        if(k>=nums[i])pick = f(i,k-nums[i],nums,dp);
+        int npick = f(i-1,k,nums,dp);
+        return dp[i][k] = pick+npick;
+    }
     int change(int k, vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        for(int i = 0;i<n;i++){
-            dp[i][0] = 1;
-        }
-        for(int i = 0;i<=k;i++){
-            if(i%nums[0] == 0){
-                dp[0][i] = 1;
-            }else{
-                dp[0][i] = 0;
-            }
-        }
-        for(int i = 1;i<n;i++){
-            for(int j = 1;j<=k;j++){
-                int np = dp[i-1][j];
-                int p = 0;
-                if(j>=nums[i])p = dp[i][j-nums[i]];
-        
-                dp[i][j] = p+np;
-            }
-        }
-        
-        return dp[n-1][k];
+        return f(n-1,k,nums,dp);
     }
 };
