8460fc93f2fbecf42ca87dddf03f523ced11e701
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-10 02:22:43 +0530

    Time: 92 ms (30.36%), Space: 96.3 MB (16.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,12 +3,13 @@ public:
     int maxProfit(vector<int>& nums) {
         int n = nums.size();
         int maxi = INT_MIN;
-        int minCost = nums[0];
+        int last = nums[0];
         for(int i = 0;i<n;i++){
-            int profit = nums[i]-minCost;
+            int profit = nums[i]-last;
             maxi = max(profit,maxi);
-            minCost = min(minCost,nums[i]);
+            last = min(last,nums[i]);
         }
+        
         return maxi;
     }
 };
