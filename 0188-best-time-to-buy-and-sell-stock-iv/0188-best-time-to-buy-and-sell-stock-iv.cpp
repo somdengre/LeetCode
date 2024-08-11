9714ecd89c7f377cfb5f9aca2998bfad111503f7
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-12 01:07:49 +0530

    Time: 22 ms (17.09%), Space: 17 MB (21.18%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-//     int f(int i,int buy,int cap,vector<int>&nums,vector<vector<vector<int>>>&dp){
-//         if(cap == 0)return 0;
-//         if(i == nums.size())return 0;
-//         if(dp[i][buy][cap] != -1)return dp[i][buy][cap];
+    int f(int i,int b,vector<int>&nums,int k,vector<vector<vector<int>>>&dp){
+        if(k == 0)return 0;
+        if(i == nums.size())return 0;
+        if(dp[i][b][k] != -1)return dp[i][b][k];
         
-//         if(buy){
-//             return dp[i][buy][cap] = max(-nums[i] + f(i+1,0,cap,nums,dp),0+f(i+1,1,cap,nums,dp));
-//         }else{
-//             return dp[i][buy][cap] = max(nums[i] + f(i+1,1,cap-1,nums,dp),f(i+1,0,cap,nums,dp));
-//         }
-//     }
+        int profit = 0;
+        if(b){
+            profit = max(f(i+1,1,nums,k,dp), -nums[i] + f(i+1,0,nums,k,dp));
+        }else{
+            profit = max(f(i+1,0,nums,k,dp), nums[i] + f(i+1,1,nums,k-1,dp));
+         }
+        
+        return dp[i][b][k] =  profit;
+    } 
     int maxProfit(int k, vector<int>& nums) {
         int n = nums.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
-        
-        for(int i = n-1;i>=0;i--){
-            for(int buy = 0;buy<=1;buy++){
-                for(int cap = 1;cap<=k;cap++){
-                     if(buy){
-                        dp[i][buy][cap] = max(-nums[i] + dp[i+1][0][cap] ,dp[i+1][1][cap]);
-                    }else{
-                        dp[i][buy][cap] = max(nums[i] + dp[i+1][1][cap-1] ,dp[i+1][0][cap]);
-                    }
-                }
-            }
-        }
-        return dp[0][1][k];
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return f(0,1,nums,k,dp);
     }
 };
