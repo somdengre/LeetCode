be243dd2d00429065b7658b3cdb4201cbd57a542
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-12 01:44:13 +0530

    Time: 9 ms (17.40%), Space: 14 MB (78.87%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
-//     int f(int i,int b,vector<int>&nums,vector<vector<int>>&dp){
-//         if(i >= nums.size())return 0;
-//         if(dp[i][b] != -1)return dp[i][b];
-        
-//         int profit = 0;
-//         if(b){
-//             profit = max(f(i+1,1,nums,dp),-nums[i]+ f(i+1,0,nums,dp));
-//         }else{
-//             profit = max(f(i+1,0,nums,dp),nums[i] + f(i+2,1,nums,dp));
-//         }
-        
-//         return dp[i][b] = profit;
-        
-//     }
+
     int maxProfit(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        vector<int>prev(2,0),prev1(2,0);
         
         
         for(int i = n-1;i>=0;i--){
+            vector<int>temp(2,0);
             for(int b = 0;b<=1;b++){
                 int profit = 0;
                 if(b){
-                    profit = max(dp[i+1][1],-nums[i]+ dp[i+1][0]);
+                    profit = max(prev[1],-nums[i]+ prev[0]);
                 }else{
-                    profit = max(dp[i+1][0],nums[i]+dp[i+2][1]);
+                    profit = max(prev[0],nums[i]+prev1[1]);
                     
                 }
         
-                 dp[i][b] = profit;
+                temp[b] = profit;
             }
+            prev1 = prev;
+            prev = temp;
         }
-        return dp[0][1];
+        return prev[1];
     }
 };
