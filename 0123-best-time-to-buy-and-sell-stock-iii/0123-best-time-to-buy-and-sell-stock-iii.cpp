14f870ac7b93e9701319160f2529a1f97c5db4eb
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-12 01:28:16 +0530

    Time: 577 ms (5.03%), Space: 221.6 MB (35.54%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
-    int f(int i,int b,vector<int>&nums,int t,vector<vector<vector<int>>>&dp){
-        if(t == 0)return 0;
-        if(i == nums.size())return 0;
-        if(dp[i][b][t] != -1)return dp[i][b][t];
-        int profit = 0;
-        if(b){
-            profit = max(f(i+1,1,nums,t,dp),-nums[i] + f(i+1,0,nums,t,dp));
-        }else{
-            profit = max(f(i+1,0,nums,t,dp),nums[i]+f(i+1,1,nums,t-1,dp));
-         }
-        
-        return dp[i][b][t] = profit;
-    }
+    
     int maxProfit(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        for(int b = 0;b<=1;b++){
-            for(int t = 0;t<=2;t++){
-                dp[n][b][t] = 0;
-            }
-        }
+        vector<vector<int>>prev(2,vector<int>(3,0));
         
-        for(int i = 0;i<=n;i++){
-            for(int b = 0;b<=1;b++){
-                dp[i][b][0] = 0;
-            }
-        }
         
         for(int i = n-1;i>=0;i--){
+            vector<vector<int>>temp(2,vector<int>(3,0));
             for(int b = 0;b<=1;b++){
                 for(int t = 1;t<=2;t++){
                     int profit = 0;
                     if(b){
-                        profit = max(dp[i+1][1][t] ,-nums[i] + dp[i+1][0][t]);
+                        profit = max(prev[1][t] ,-nums[i] + prev[0][t]);
                     }else{
-                        profit = max(dp[i+1][0][t] ,nums[i]+dp[i+1][1][t-1]);
+                        profit = max(prev[0][t] ,nums[i]+prev[1][t-1]);
                     }
         
-                    dp[i][b][t] = profit;
+                    temp[b][t] = profit;
                 }
             }
+            prev = temp;
         }
-        return dp[0][1][2];
+        return prev[1][2];
     }
 };
