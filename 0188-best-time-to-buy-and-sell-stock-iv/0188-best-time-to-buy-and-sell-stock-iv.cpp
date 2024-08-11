e46518b7f74c75de302d73f90560aaed3dae2211
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-12 01:30:29 +0530

    Time: 3 ms (95.23%), Space: 17.9 MB (13.27%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,22 +3,24 @@ public:
  
     int maxProfit(int t, vector<int>& nums) {
         int n = nums.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(t+1,0)));
+        vector<vector<int>>prev(2,vector<int>(t+1,0));;
         
         for(int i = n-1;i>=0;i--){
+            vector<vector<int>>temp(2,vector<int>(t+1,0));
             for(int b = 0;b<=1;b++){
                 for(int k = 1;k<=t;k++){
                     int profit = 0;
                     if(b){
-                        profit = max(dp[i+1][1][k] , -nums[i] + dp[i+1][0][k]);
+                        profit = max(prev[1][k] , -nums[i] + prev[0][k]);
                     }else{
-                        profit = max(dp[i+1][0][k] , nums[i] + dp[i+1][1][k-1]);
+                        profit = max(prev[0][k] , nums[i] +prev[1][k-1]);
                     }
         
-                    dp[i][b][k] =  profit;
+                    temp[b][k] =  profit;
                 }
             }
+            prev = temp;
         }
-        return dp[0][1][t];
+        return prev[1][t];
     }
 };
