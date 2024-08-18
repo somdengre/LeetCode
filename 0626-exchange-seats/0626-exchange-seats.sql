9c33012e80eb9820592a9953d415054adbc1fe96
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-19 04:25:23 +0530

    Time: 324 ms (61.97%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,5 +1,10 @@
+
+
+
 select if(id < (select max(id) from Seat),
          if(id%2 = 0,id-1,id+1),
-          if(id%2 = 0, id-1,id)
-         ) as id, student
+          if(id%2 = 0,id-1,id)
+         ) as id,student
 from Seat order by id;
+
+
