3735dd1c6af5bcd76ddd79e6f81a4ae6f96ef2a1
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-28 00:09:08 +0530

    Time: 122 ms (82.58%), Space: 70.2 MB (56.01%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<pair<int,double>> adj[n];
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& pro, int start_node, int end_node) {
+        vector<vector<pair<int,double>>>adj(n);
+        for(int i = 0;i<edges.size();i++){
+            adj[edges[i][0]].push_back({edges[i][1],pro[i]});
+            adj[edges[i][1]].push_back({edges[i][0],pro[i]});
         }
-        vector<double> prob(n,0.0);
-        prob[start]=1.0;
-        queue<pair<int,double>> q;
-        q.push({start,1.0});
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            for(auto v:adj[it.first]){
-                if(prob[v.first]<(it.second*v.second)){
-                    prob[v.first]=it.second*v.second;
-                    q.push({v.first,prob[v.first]});
+        vector<double>dis(n,0);
+        dis[start_node] = 1;
+        priority_queue<pair<double, int>> pq;
+        pq.push({1,start_node});
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int node = it.second;
+            double dist = (double)it.first;
+            if(node == end_node)return dist;
+            for(auto it: adj[node]){
+                int adjnode = it.first;
+                double wt = (double)it.second;
+                if(wt*dist > dis[adjnode]){
+                    dis[adjnode] = (double)wt*dist;
+                    pq.push({dis[adjnode],adjnode});
                 }
             }
         }
-        return prob[end];
-       
+        
+        return dis[end_node];
     }
 };
