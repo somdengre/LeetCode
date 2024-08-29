680c24ca0196aacd14208dd78b71a95ac583a3d7
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-08-30 03:10:38 +0530

    Time: 7 ms (99.16%), Space: 27 MB (70.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,24 +3,25 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
         int cnt = 0;
-        int el = 0;
+        int el = -1;
+        
         for(int i = 0;i<n;i++){
             if(cnt == 0){
                 el = nums[i];
                 cnt++;
-            }else if(nums[i] != el){
-                cnt--;
-            }else{
+            }else if(nums[i] == el){
                 cnt++;
+            }else{
+                cnt--;
             }
         }
-        cnt = 0;
+        
         for(int i = 0;i<n;i++){
             if(nums[i] == el){
                 cnt++;
             }
         }
-        if(cnt > (n/2))return el;
-        return 0;
+        if(cnt >= n/2)return el;
+        return el;
     }
 };
