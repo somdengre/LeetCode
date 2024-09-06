3463802ebb7b7b1ee06f3dd8bcf16daf8a0947e5
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-09-06 17:04:59 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (5.03%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -13,26 +13,21 @@ public:
         return f(i+1,s,temp,neg);
     }
     int myAtoi(string s) {
-       int n = s.length();
+        int n = s.length();
         int i = 0;
         
         while(i<n && s[i] == ' '){
             i++;
         }
         int neg = 1;
-        if(i< n && s[i] == '-' || s[i] == '+'){
+        if(s[i] == '-' || s[i] == '+'){
             if(s[i] == '-'){
                 neg = -1;
             }
             i++;
         }
+        
         long long temp = 0;
-        // while(i<n && s[i] >= '0' && s[i] <= '9'){
-        //     temp = temp*10 + (s[i]-'0');
-        //     i++;
-        //     if(neg == -1 && -temp < INT_MIN)return INT_MIN;
-        //     if(neg == 1 && temp > INT_MAX)return INT_MAX;
-        // }
         f(i,s,temp,neg); 
         if(neg == -1 && -temp <= INT_MIN)return INT_MIN;
         if(neg == 1 && temp >= INT_MAX )return INT_MAX;
