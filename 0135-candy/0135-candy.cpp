74839eec65e19ca5fae0ab82e804cff3f9b407d0
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-09-10 17:16:48 +0530

    Time: 16 ms (49.01%), Space: 21 MB (10.01%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,35 +2,27 @@ class Solution {
 public:
     int candy(vector<int>& nums) {
         int n = nums.size();
-        int sum = n;
-        int i = 1;
+        vector<int>left(n,0),right(n,0);
+        left[0] = 1;
+        right[n-1] = 1;
         
-        while(i<n){
-            
-            if(nums[i] == nums[i-1]){
-                i++;
-                continue;
+        for(int i = 1;i<n;i++){
+            if(nums[i] > nums[i-1]){
+                left[i] = left[i-1]+1;
+            }else{
+                left[i] = 1;
             }
-            
-            int peak = 0;
-            
-            while(i<n && nums[i-1] < nums[i]){
-                peak++;
-                sum+=peak;
-                i++;
+        }
+        for(int i = n-2;i>=0;i--){
+            if(nums[i] > nums[i+1]){
+                right[i] = right[i+1]+1;
+            }else{
+                right[i] = 1;
             }
-            int down = 0;
-            while(i<n && nums[i-1]>nums[i]){
-                down++;
-                sum+=down;
-                i++;
-            }
-            
-            sum-=min(down,peak);
         }
         
-        return sum;
-        
-        
+        int ans = 0;
+        for(int i = 0;i<n;i++)ans+=max(left[i],right[i]);
+        return ans;
     }
 };
