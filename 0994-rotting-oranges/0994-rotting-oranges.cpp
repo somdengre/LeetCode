f42dce0a9eb37c0c8fb5efff8818509102648e47
Som Dengre <115890508+somdengre@users.noreply.github.com>
2024-09-21 03:25:50 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (48.41%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
+    int orangesRotting(vector<vector<int>>& nums) {
+        int n = nums.size();
+        int m = nums[0].size();
+        int cnt = 0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>>q;
-        int vis[n][m];
         
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(grid[i][j] == 2){
+                if(nums[i][j] == 2){
                     q.push({{i,j},0});
                     vis[i][j] = 2;
                 }else{
@@ -17,44 +17,32 @@ public:
                 }
             }
         }
-        
-        int tm = 0;
-        int drow[] = {-1,0,+1,0};
-        int dcol[] = {0,1,0,-1};
-        
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
-            
             q.pop();
-            tm = max(t,tm);
-            
+            cnt = max(cnt,t);
             for(int i = 0;i<4;i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
+                int nrow = r+delrow[i];
+                int ncol = c+delcol[i];
                 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1 ){
+                if(nrow >= 0 && nrow <n && ncol >=0 && ncol <m && vis[nrow][ncol] == 0 && nums[nrow][ncol] == 1){
                     q.push({{nrow,ncol},t+1});
                     vis[nrow][ncol] = 2;
                 }
             }
-            
-            
         }
         
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(vis[i][j] != 2 && grid[i][j] == 1){
+                if(vis[i][j] != 2 && nums[i][j] == 1){
                     return -1;
                 }
             }
         }
-        
-        return tm;
-        
-        
-        
-        
+        return cnt;
     }
 };
